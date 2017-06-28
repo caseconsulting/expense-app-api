c24acfe72ded1896967bffb71e13aeb8dc86ae66
Jacob <jacobboisseau@gmail.com>
2017-06-28 10:10:38 -0400

    server-setup: working route with no params working

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var expenseType = require('./routes/expenseType');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
+app.use('/create-expense-type/:id',expenseType);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/expenseType.js
+++ routes/expenseType.js
@@ -3,9 +3,22 @@ var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.put('/', function(req, res) {
+  res.sendStatus(200);
+  //console.log(req.params.id);
+  addExpense(req.params.id, req.params.name, req.params.budget, req.params.overdraftFlag);
 });
+
+var expenseTypes=[];
+
+function addExpense(newId, newName, newBudget, newOverdraftFlag) {
+  //the inputs will be checked on client site before being passed
+  console.log('id: '+ newId +' name: '+newName +' budget: '+newBudget +' overdraftFlag: '+newOverdraftFlag);
+  var newExpense = { id: newId, name: newName, budget: newBudget, overdraftFlag: newOverdraftFlag };
+  expenseTypes.push(newExpense);
+  console.log(expenseTypes);
+}
+
 
 module.exports = router;
 //remember to add this to app.js so that it can use the routes in this file
