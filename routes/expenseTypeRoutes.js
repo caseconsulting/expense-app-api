3e6a5b86b4c4d7eccee3bafd1ca901233f3a054f
Jacob <jacobboisseau@gmail.com>
2017-06-28 14:41:49 -0400

    adding-update-route: added delete route and send message with code back

--- routes/expenseTypeRoutes.js
+++ routes/expenseTypeRoutes.js
@@ -8,20 +8,24 @@ router.put('/create/:id/:name/:budget/:over', function(req, res) {
   console.log(req.params);
   let newExpenseType = expenseTypes.addExpense(input.id, input.name, input.budget, input.odFlag);
   jsonModify.addToJson(newExpenseType);
-  res.sendStatus(200);
+  res.status(200).send('created ID: '+input.id);
 });
-
+//read route
 router.get('/read/:id', function(req, res) {
   console.log("get request recieved");
-  res.status(200).send('request recieved for id' + req.params.id);
+  res.status(200).send('ID to find: ' + req.params.id);
 });
 //update route
 router.put('/update/:id',function(req,res){
   let id = req.params.id;
   console.log(id);
-  res.status(200).send('ID recived: '+id);
+  res.status(200).send('ID to update: '+id);
 });
 //delete route
-
+router.delete('/delete/:id',function(req,res){
+  let id = req.params.id;
+  console.log(id);
+  res.status(200).send('ID to delete: '+id);
+});
 
 module.exports = router;
