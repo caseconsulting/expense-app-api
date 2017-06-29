1aa7d6c1c92be86174e70171cd80f40756bebc9a
Fahad <fahada95@vt.edu>
2017-06-29 09:01:50 -0400

    expenseTypeExpansion: moved json file setup to the top

--- js/jsonModify.js
+++ js/jsonModify.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
-
+let filePath = 'json/mock_json.json';
+let jsonFile = fs.readFileSync(filePath);
+let jsonParsed = JSON.parse(jsonFile);
 //read in the json file
 //parse existing json to an array
 //push new value to array
@@ -7,19 +9,16 @@ var fs = require('fs');
 //overwrite json
 //TODO -> fix naming convention
 var addToJson = function(newExpenseType){
-  let filePath = 'json/mock_json.json';
-  let jsonFile = fs.readFileSync(filePath);
-  let jsonParsed = JSON.parse(jsonFile);
+
   console.log(jsonParsed);
   jsonParsed.push(newExpenseType);
   var arrayJSON = JSON.stringify(jsonParsed, null, 2);
   fs.writeFileSync(filePath, arrayJSON);
 }
 
+//iterate through the array
+//find the object with the passed in ID
 var findObjectPosition = function(passedID){
-  let filePath = 'json/mock_json.json';
-  let jsonFile = fs.readFileSync(filePath);
-  let jsonParsed = JSON.parse(jsonFile);
   for (let i = 0; i<jsonParsed.length; i++)
   {
     if (jsonParsed[i].id == passedID)
@@ -28,19 +27,14 @@ var findObjectPosition = function(passedID){
     }
   }
   console.log("No object found with matching ID");
-
 }
 
 //read in the json file
 //parse existing json to an array
 //iterate through the json and find the appropriate value and return it
 var readFromJson = function(passedID){
-  let filePath = 'json/mock_json.json';
-  let jsonFile = fs.readFileSync(filePath);
-  let jsonParsed = JSON.parse(jsonFile);
   let position = findObjectPosition(passedID);
   return jsonParsed[position];
-
 }
 
 jsonModify = {
