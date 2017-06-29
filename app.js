635d87360c84b86e37a0c7d95d4d5edd015f503f
Fahad <fahada95@vt.edu>
2017-06-29 12:01:04 -0400

    expenseTypeExpansion: outsourced route functions

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/expense-type',expenseTypeRoutes);
+app.use('/expense-type',expenseTypeRoutes.router);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/expenseTypeRoutes.js
+++ routes/expenseTypeRoutes.js
@@ -3,42 +3,45 @@ const router = express.Router();
 const expenseTypes = require('../js/expenseTypes');
 const jsonModify = require('../js/jsonModify');
 /* create expense type listing. */
-router.post('/', function(req, res) {
+function create(req, res) {
   //response is passed in body
-
   const newExpenseType = expenseTypes.add(req.body);
   //const newExpenseType = expenseTypes.addExpense(input.name, input.budget, input.odFlag);
   jsonModify.addToJson(newExpenseType);
   res.status(200).send(newExpenseType);
-});
-//read route
-router.get('/:id', function(req, res) {
+}
+
+function read(req, res) {
   console.log("get request recieved");
   const output = jsonModify.readFromJson(req.params.id);
   res.status(200).send(output);
-});
+}
 
-router.get('/', function(req, res) {
+function showList(req, res) {
   console.log("get request recieved for everything");
   const output = jsonModify.getJson();
   res.status(200).send(output);
-});
+}
 
-//update route
-router.put('/:id',function(req,res){
+function update(req,res){
   const input = req.body;
   console.log(input);
   const newExpenseType = expenseTypes.update(req.params.id, req.body);
   jsonModify.updateJsonEntry(newExpenseType);
   res.status(200).send(newExpenseType);
-});
+}
 
-//delete route
-router.delete('/:id',function(req,res){
+function onDelete(req,res){
 
   const id = req.params.id;
   const output = jsonModify.removeFromJson(id);
   res.status(200).send(output);
-});
+}
 
-module.exports = router;
+router.get('/', showList);
+router.post('/', create);
+router.get('/:id', read);
+router.put('/:id', update);
+router.delete('/:id', onDelete);
+
+module.exports = {create, onDelete, read, router, showList, update };
