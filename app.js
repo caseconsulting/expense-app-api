261774fe4c0fc32c5d0fb203ae5c4b103d4e8845
Jacob <jacobboisseau@gmail.com>
2017-06-30 10:03:02 -0400

    added final route to app.js and tested route

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var expenseTypeRoutes = require('./routes/expenseTypeRoutes');
-
+var employee = require('./routes/employeeRoutes');
 var app = express();
 
 // view engine setup
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/expense-type',expenseTypeRoutes.router);
+app.use('/employee',employee.router);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
