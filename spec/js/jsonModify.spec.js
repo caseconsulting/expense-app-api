6ad9a200dd13773829612f66ba30c5baabc4e501
Jacob <jacobboisseau@gmail.com>
2017-07-05 13:07:27 -0400

    jas-adding: added real time updates for jasmine

--- spec/js/jsonModify.spec.js
+++ spec/js/jsonModify.spec.js
@@ -14,30 +14,30 @@ describe("JsonModify", ()=> {
   });
 
   describe("_matches",()=>{
-      let id,jsonObj;
-      beforeEach(()=>id = "{id}");
-      beforeEach(()=>jsonObj={});
+    let id,jsonObj;
+    beforeEach(()=>id = "{id}");
+    beforeEach(()=>jsonObj={});
 
-      describe("when the json id matches",()=>{
-        beforeEach(()=>jsonObj.id=id);
+    describe("when the json id matches",()=>{
+      beforeEach(()=>jsonObj.id=id);
 
-        it("should be true",()=>{
-          const f = jsonModify._matches(id);
-          expect(f(jsonObj)).toEqual(true);
-        });
-
+      it("should be true",()=>{
+        const f = jsonModify._matches(id);
+        expect(f(jsonObj)).toEqual(true);
       });
-      describe("when the json id matches",()=>{
-        beforeEach(()=>jsonObj.id="{NOTid}");
 
-        it("should be false",()=>{
-          const f = jsonModify._matches(id);
-          expect(f(jsonObj)).toEqual(false);
-        });
+    });
+    describe("when the json id matches",()=>{
+      beforeEach(()=>jsonObj.id="{NOTid}");
 
+      it("should be false",()=>{
+        const f = jsonModify._matches(id);
+        expect(f(jsonObj)).toEqual(false);
       });
-    }); // _matches
-  });
+
+    });
+  }); // _matches
+
 
   describe("_writeCallback",()=>{
 
@@ -130,88 +130,89 @@ describe("JsonModify", ()=> {
       });
     }); // readFromJson
 
-  describe("removeFromJson",()=>{
-    let passedID, callback;
-    beforeEach(()=>{
-      callback = jasmine.createSpy("callback");
-    });
-    beforeEach(()=>passedID="{passedID}");
-    describe("when matches", ()=>{
+    describe("removeFromJson",()=>{
+      let passedID, callback;
       beforeEach(()=>{
-        spyOn(jsonModify,"_matches").and.returnValue("{matches}");
+        callback = jasmine.createSpy("callback");
       });
-      afterEach(()=>{
-        expect(jsonModify._matches).toHaveBeenCalledWith(passedID);
-      });
-      describe("when find Index returns -1",()=>{
-        beforeEach(()=> spyOn(_,"findIndex").and.returnValue(-1));
-        afterEach(()=> expect(_.findIndex).toHaveBeenCalledWith(jsonParsed,"{matches}"));
-
-        it("should call the error function callback",()=>{
-          //callback function should be called
-          jsonModify.removeFromJson(passedID,callback);
-          expect(callback).toHaveBeenCalledWith({message:'REMOVE: Object not found'});
+      beforeEach(()=>passedID="{passedID}");
+      describe("when matches", ()=>{
+        beforeEach(()=>{
+          spyOn(jsonModify,"_matches").and.returnValue("{matches}");
         });
-      });
-      describe("when find Index returns a position",()=>{
-        beforeEach(()=> spyOn(_,"findIndex").and.returnValue(1));
-        afterEach(()=> expect(_.findIndex).toHaveBeenCalledWith(jsonParsed,"{matches}"));
+        afterEach(()=>{
+          expect(jsonModify._matches).toHaveBeenCalledWith(passedID);
+        });
+        describe("when find Index returns -1",()=>{
+          beforeEach(()=> spyOn(_,"findIndex").and.returnValue(-1));
+          afterEach(()=> expect(_.findIndex).toHaveBeenCalledWith(jsonParsed,"{matches}"));
 
-        describe("when the object to be removed is found",()=>{
-          beforeEach(()=> spyOn(_,"find").and.returnValue('output'));
-          afterEach(()=> expect(_.find).toHaveBeenCalledWith(jsonParsed,"{matches}"));
-          describe("when object is removed",()=>{
-            beforeEach(()=> spyOn(_,"reject").and.returnValue(['jsonParsed - removed']));
-            afterEach(()=> expect(_.reject).toHaveBeenCalledWith(jsonParsed, '{matches}'));
+          it("should call the error function callback",()=>{
+            //callback function should be called
+            jsonModify.removeFromJson(passedID,callback);
+            expect(callback).toHaveBeenCalledWith({message:'REMOVE: Object not found'});
+          });
+        });
+        describe("when find Index returns a position",()=>{
+          beforeEach(()=> spyOn(_,"findIndex").and.returnValue(1));
+          afterEach(()=> expect(_.findIndex).toHaveBeenCalledWith(jsonParsed,"{matches}"));
 
-            describe("when writeCallback is called",()=>{
-              beforeEach(()=> spyOn(jsonModify,"_writeCallback").and.returnValue('callback()'));
-              afterEach(()=> expect(jsonModify._writeCallback).toHaveBeenCalledWith('output',callback));
-              describe("when writting to json",()=>{
-                beforeEach(()=> spyOn(fs,"writeFile"));
-                afterEach(()=> expect(fs.writeFile).toHaveBeenCalledWith('json/test.json',JSON.stringify(['jsonParsed - removed'], null, 2),'callback()'));
+          describe("when the object to be removed is found",()=>{
+            beforeEach(()=> spyOn(_,"find").and.returnValue('output'));
+            afterEach(()=> expect(_.find).toHaveBeenCalledWith(jsonParsed,"{matches}"));
+            describe("when object is removed",()=>{
+              beforeEach(()=> spyOn(_,"reject").and.returnValue(['jsonParsed - removed']));
+              afterEach(()=> expect(_.reject).toHaveBeenCalledWith(jsonParsed, '{matches}'));
 
-                it("should will write the file and return the removed element",()=>{
-                  jsonModify.removeFromJson(passedID,callback);
+              describe("when writeCallback is called",()=>{
+                beforeEach(()=> spyOn(jsonModify,"_writeCallback").and.returnValue('callback()'));
+                afterEach(()=> expect(jsonModify._writeCallback).toHaveBeenCalledWith('output',callback));
+                describe("when writting to json",()=>{
+                  beforeEach(()=> spyOn(fs,"writeFile"));
+                  afterEach(()=> expect(fs.writeFile).toHaveBeenCalledWith('json/test.json',JSON.stringify(['jsonParsed - removed'], null, 2),'callback()'));
+
+                  it("should will write the file and return the removed element",()=>{
+                    jsonModify.removeFromJson(passedID,callback);
+                  });
                 });
               });
-            });
 
+            });
           });
         });
       });
-    });
-  }); // removeFromJson
+    }); // removeFromJson
 
-  describe("updateJsonEntry",()=>{
-    let newJsonObj, callback;
-    beforeEach(()=>{
-      callback = jasmine.createSpy('callback');
-    });
-    beforeEach(()=>newJsonObj = {id:'{id}'});
+    describe("updateJsonEntry",()=>{
+      let newJsonObj, callback;
+      beforeEach(()=>{
+        callback = jasmine.createSpy('callback');
+      });
+      beforeEach(()=>newJsonObj = {id:'{id}'});
 
-    describe("when _addToJson is called",()=>{
-      beforeEach(()=> spyOn(jsonModify,"_addToJson").and.returnValue('addToJson()'));
-      afterEach(()=> expect(jsonModify._addToJson).toHaveBeenCalledWith(newJsonObj,callback));
+      describe("when _addToJson is called",()=>{
+        beforeEach(()=> spyOn(jsonModify,"_addToJson").and.returnValue('addToJson()'));
+        afterEach(()=> expect(jsonModify._addToJson).toHaveBeenCalledWith(newJsonObj,callback));
 
-      describe("when removeFromJson is called",()=>{
-        beforeEach(()=> spyOn(jsonModify,"removeFromJson"));
-        afterEach(()=> expect(jsonModify.removeFromJson).toHaveBeenCalledWith(newJsonObj.id,'addToJson()'));
+        describe("when removeFromJson is called",()=>{
+          beforeEach(()=> spyOn(jsonModify,"removeFromJson"));
+          afterEach(()=> expect(jsonModify.removeFromJson).toHaveBeenCalledWith(newJsonObj.id,'addToJson()'));
 
-        it("should pass along to removeFromJson",()=>{
-          jsonModify.updateJsonEntry(newJsonObj,callback);
+          it("should pass along to removeFromJson",()=>{
+            jsonModify.updateJsonEntry(newJsonObj,callback);
+          });
         });
+
       });
 
-    });
 
+    }); // updateJsonEntry
 
-  }); // updateJsonEntry
+    describe("getJson", ()=>{
+      it("should return a json filePath", () => {
+        expect(jsonModify.getJson()).toEqual([]);
+      });
+    }); // getJson
 
-  describe("getJson", ()=>{
-    it("should return a json filePath", () => {
-      expect(jsonModify.getJson()).toEqual([]);
-    });
-  }); // getJson
-
+  });
 });
