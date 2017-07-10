3cd6a7f076337a880c66c3a21361bcdd6f969b7c
Jacob <jacobboisseau@gmail.com>
2017-07-10 10:48:18 -0400

    jas-adding: added expense routes and functions

---
+++ routes/expenseRoutes.js
@@ -0,0 +1,59 @@
+const jsonModify = require('../js/jsonModify')('expense.json');
+const uuid = require('uuid/v4');
+
+function _add(uuid,{
+expenseId,
+purchaseDate,
+reimbursedDate,
+cost,
+description,
+note,
+receipt,
+expenseTypeId,
+userId
+}) {
+  return {
+    id: uuid,
+    expenseId,
+    purchaseDate,
+    reimbursedDate,
+    cost,
+    description,
+    note,
+    receipt,
+    expenseTypeId,
+    userId
+  };
+}
+
+function _update(id, {
+  expenseId,
+  purchaseDate,
+  reimbursedDate,
+  cost,
+  description,
+  note,
+  receipt,
+  expenseTypeId,
+  userId
+}) {
+  console.log(id, firstName, middleName, lastName, empId, hireDate);
+  return {
+    id,
+    expenseId,
+    purchaseDate,
+    reimbursedDate,
+    cost,
+    description,
+    note,
+    receipt,
+    expenseTypeId,
+    userId
+  };
+}
+
+const crud = require('./crudRoutes')(jsonModify, _add, _update, uuid());
+module.exports = Object.assign({}, crud, {
+  _add,
+  _update
+});
