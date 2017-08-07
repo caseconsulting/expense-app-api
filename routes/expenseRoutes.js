42833939f3de413363bddb109ad6765afaa1e437
Fahad <fahada95@vt.edu>
2017-08-07 14:05:59 -0400

    testing-additions: added validation for expenses

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,24 +1,13 @@
 const Crud = require('./crudRoutes');
-
-class ExpenseRoutes extends Crud{
+const JsonModify = require('../js/jsonModify');
+class ExpenseRoutes extends Crud {
   constructor(jsonModify, uuid) {
     super(jsonModify, uuid);
     this.jsonModify = jsonModify;
   }
 
 
- _add(uuid,{
-purchaseDate,
-reimbursedDate,
-cost,
-description,
-note,
-receipt,
-expenseTypeId,
-userId
-}) {
-  return {
-    id: uuid,
+  _add(uuid, {
     purchaseDate,
     reimbursedDate,
     cost,
@@ -27,21 +16,26 @@ userId
     receipt,
     expenseTypeId,
     userId
-  };
-}
+  }) {
+    const expenseIs = this.validateCostToBudget(expenseTypeId, cost);
+    if (expenseIs.valid) {
+      return {
+        id: uuid,
+        purchaseDate,
+        reimbursedDate,
+        cost,
+        description,
+        note,
+        receipt,
+        expenseTypeId,
+        userId
+      };
+    } else {
+      return expenseIs;
+    }
+  }
 
- _update(id, {
-  purchaseDate,
-  reimbursedDate,
-  cost,
-  description,
-  note,
-  receipt,
-  expenseTypeId,
-  userId
-}){
-  return {
-    id,
+  _update(id, {
     purchaseDate,
     reimbursedDate,
     cost,
@@ -50,7 +44,43 @@ userId
     receipt,
     expenseTypeId,
     userId
-  };
-}
+  }) {
+    return {
+      id,
+      purchaseDate,
+      reimbursedDate,
+      cost,
+      description,
+      note,
+      receipt,
+      expenseTypeId,
+      userId
+    };
+  }
+
+  validateCostToBudget(expenseTypeId, cost) {
+    //new instance
+    const expenseTypeJson = new JsonModify('expenseType.json');
+    const expenseType = expenseTypeJson._specificFind("id", expenseTypeId);
+    const remainingBudget = expenseType.budget - cost;
+
+    if (remainingBudget < 0 && expenseTypeId.odFlag) {
+      return {
+        valid: true,
+        remainingBudget: remainingBudget
+      };
+    } else if (remainingBudget >= 0) {
+      return {
+        valid: true,
+        remainingBudget: remainingBudget
+      };
+    } else {
+      return {
+        valid: false,
+        remainingBudget: remainingBudget
+      };
+    }
+
+  }
 }
 module.exports = ExpenseRoutes;
