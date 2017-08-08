6d0e9af363f03dc397681e6772ac390cb80dd7d4
Fahad <fahada95@vt.edu>
2017-08-08 14:50:52 -0400

    testing-additions: update

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -6,9 +6,9 @@ class ExpenseRoutes extends Crud {
     this.jsonModify = jsonModify;
   }
 
-  _delete(id){
-      const fe = this.jsonModify._specificFind("id", id);
-      this.deleteCostFromBudget(fe.expenseTypeId, fe.userId, fe.cost);
+  _delete(id) {
+    const fe = this.jsonModify._specificFind("id", id);
+    this.deleteCostFromBudget(fe.expenseTypeId, fe.userId, fe.cost);
   }
 
   _add(uuid, {
@@ -48,9 +48,12 @@ class ExpenseRoutes extends Crud {
     receipt,
     expenseTypeId,
     userId
-  }) {
+  })
+  this.deleteCostFromBudget(expenseTypeId, userId, cost);
+  const expenseIs = this.validateCostToBudget(expenseTypeId, userId, cost);
+  if (expenseIs.valid) {
     return {
-      id,
+      id: uuid,
       purchaseDate,
       reimbursedDate,
       cost,
@@ -60,87 +63,81 @@ class ExpenseRoutes extends Crud {
       expenseTypeId,
       userId
     };
+  } else {
+    return expenseIs;
+  }
+}
+
+validateCostToBudget(expenseTypeId, userId, cost) {
+  //new instance
+  const expenseTypeJson = new JsonModify('expenseType.json');
+  const employeeJson = new JsonModify('employee.json');
+  const expenseType = expenseTypeJson._specificFind("id", expenseTypeId);
+  const employee = employeeJson._specificFind("empId", '' + userId);
+  let employeeBalance;
+  let budgetPosition;
+  let remaining;
+
+  for (var i = 0; i < employee.expenseTypes.length; i++) {
+    if (employee.expenseTypes[i].id === expenseTypeId) {
+      budgetPosition = i;
+      employeeBalance = +employee.expenseTypes[i].balance + cost;
+      remaining = expenseType.budget - employeeBalance;
+    } else {
+      //create new balance under the employee
+    }
+  }
+  //OVERDRAFT
+  if (expenseType.budget - employeeBalance < 0 && expenseType.odFlag) {
+    employee.expenseTypes[budgetPosition].balance = '' + employeeBalance;
+    employeeJson.updateJsonEntry(employee);
+
+    return {
+      valid: true,
+      newBalance: employeeBalance
+    };
+  }
+  //PARTIAL COVERAGE
+  else if (expenseType.budget !== +employee.expenseTypes[budgetPosition].balance && expenseType.budget - employeeBalance < 0 && !expenseType.odFlag && remaining < 0) {
+    employee.expenseTypes[budgetPosition].balance = '' + expenseType.budget;
+    employee.expenseTypes[budgetPosition].owedAmount = '' + Math.abs(remaining);
+    employeeJson.updateJsonEntry(employee);
+    return {
+      valid: true,
+      newBalance: employeeBalance
+    };
+  }
+  //COVERED BY BUDGET
+  else if (expenseType.budget - employeeBalance >= 0) {
+    employee.expenseTypes[budgetPosition].balance = '' + employeeBalance;
+    employeeJson.updateJsonEntry(employee);
+
+    return {
+      valid: true,
+      newBalance: employeeBalance
+    };
+  } else {
+    return {
+      valid: false,
+      newBalance: employeeBalance
+    };
   }
 
-  validateCostToBudget(expenseTypeId, userId, cost) {
+  deleteCostFromBudget(expenseTypeId, userId, cost) {
     //new instance
     const expenseTypeJson = new JsonModify('expenseType.json');
     const employeeJson = new JsonModify('employee.json');
     const expenseType = expenseTypeJson._specificFind("id", expenseTypeId);
     const employee = employeeJson._specificFind("empId", '' + userId);
     let employeeBalance;
-    let budgetPosition;
-    let remaining;
 
     for (var i = 0; i < employee.expenseTypes.length; i++) {
       if (employee.expenseTypes[i].id === expenseTypeId) {
-        budgetPosition = i;
-        employeeBalance = +employee.expenseTypes[i].balance + cost;
-        remaining = expenseType.budget - employeeBalance;
-      } else {
-        //create new balance under the employee
+        employeeBalance = +employee.expenseTypes[i].balance - cost;
+        employee.expenseTypes[i].balance = '' + employeeBalance;
+        employeeJson.updateJsonEntry(employee);
       }
     }
-    //OVERDRAFT
-    if (expenseType.budget - employeeBalance < 0 && expenseType.odFlag) {
-
-
-      employee.expenseTypes[budgetPosition].balance = '' + employeeBalance;
-
-      employeeJson.updateJsonEntry(employee);
-
-      return {
-        valid: true,
-        newBalance: employeeBalance
-
-      };
-    }
-    //PARTIAL COVERAGE
-    else if (expenseType.budget !== +employee.expenseTypes[budgetPosition].balance && expenseType.budget - employeeBalance < 0 && !expenseType.odFlag && remaining < 0) {
-      employee.expenseTypes[budgetPosition].balance = '' + expenseType.budget;
-      employee.expenseTypes[budgetPosition].owedAmount = '' + Math.abs(remaining);
-
-      employeeJson.updateJsonEntry(employee);
-
-      return {
-        valid: true,
-        newBalance: employeeBalance
-      };
-    }
-    //COVERED BY BUDGET
-    else if (expenseType.budget - employeeBalance >= 0) {
-      employee.expenseTypes[budgetPosition].balance = '' + employeeBalance;
-
-      employeeJson.updateJsonEntry(employee);
-
-      return {
-        valid: true,
-        newBalance: employeeBalance
-      };
-    } else {
-      return {
-        valid: false,
-        newBalance: employeeBalance
-      };
-    }
-
   }
-
-    deleteCostFromBudget(expenseTypeId, userId, cost) {
-      //new instance
-      const expenseTypeJson = new JsonModify('expenseType.json');
-      const employeeJson = new JsonModify('employee.json');
-      const expenseType = expenseTypeJson._specificFind("id", expenseTypeId);
-      const employee = employeeJson._specificFind("empId", '' + userId);
-      let employeeBalance;
-
-      for (var i = 0; i < employee.expenseTypes.length; i++) {
-        if (employee.expenseTypes[i].id === expenseTypeId) {
-          employeeBalance = +employee.expenseTypes[i].balance - cost;
-          employee.expenseTypes[i].balance = ''+employeeBalance;
-          employeeJson.updateJsonEntry(employee);
-        }
-      }
-    }
 }
 module.exports = ExpenseRoutes;
