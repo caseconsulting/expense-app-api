3d8304c7301f9e5c96f183ce84ac4ca1dc137f26
Jacob <jacobboisseau@gmail.com>
2017-08-11 09:46:58 -0400

    recator-branch: fixed merge conflict

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -91,12 +91,8 @@ class ExpenseRoutes extends Crud {
     const expenseTypeJson = new JsonModify('expenseType.json');
     const employeeJson = new JsonModify('employee.json');
     const expenseType = expenseTypeJson._specificFind("id", expenseTypeId);
-<<<<<<< Updated upstream
     //TODO remove empty quotes from User ID since UUID is always string
     const employee = employeeJson._specificFind("id", '' + userId);
-
-=======
-    const employee = employeeJson._specificFind("empId", '' + userId);
 console.log(employee);
     if (!employee.expenseTypes) {
       //create new balance under the employee
@@ -104,7 +100,6 @@ console.log(employee);
       console.log(employee);
       employeeJson.updateJsonEntry(employee, callback);
     }
->>>>>>> Stashed changes
     let employeeBalance;
     let budgetPosition;
     let remaining;
