11d1cb5956cb36bb4d3aecdfea25dda0b2e20391
Fahad <fahada95@vt.edu>
2018-08-07 13:44:51 -0400

    keeping origin access identity

--- nightly-script.js
+++ nightly-script.js
@@ -1,20 +1,16 @@
-let start, end;
-if (expenseType.recurringFlag) {
-  let hireDate = moment(employee.hireDate);
-  let hireYear = hireDate.year();
-  let currentYear = moment().year();
-  let yearDiff = currentYear - hireYear;
-  let fiscalStartDate = hireDate.add(yearDiff, 'years');
-  let fiscalEndDate = fiscalStartDate.add(1, 'years');
-
-  fiscalEndDate = fiscalEndDate.subtract(1, 'days');
-  start = moment(fiscalStartDate).format('MM-DD-YYYY');
-  end = moment(fiscalEndDate).format('MM-DD-YYYY');
-} else {
-  start = expenseType.fiscalStartDate;
-  end = expenseType.fiscalEndDate;
-}
-return {
-  startDate: start,
-  endDate: end
-};
+// let start, end;
+// if (expenseType.recurringFlag) {
+//   let hireDate = moment(employee.hireDate);
+//   let hireYear = hireDate.year();
+//   let currentYear = moment().year();
+//   let yearDiff = currentYear - hireYear;
+//   let fiscalStartDate = hireDate.add(yearDiff, 'years');
+//   let fiscalEndDate = fiscalStartDate.add(1, 'years');
+//
+//   fiscalEndDate = fiscalEndDate.subtract(1, 'days');
+//   start = moment(fiscalStartDate).format('MM-DD-YYYY');
+//   end = moment(fiscalEndDate).format('MM-DD-YYYY');
+// } else {
+//   start = expenseType.fiscalStartDate;
+//   end = expenseType.fiscalEndDate;
+// }
