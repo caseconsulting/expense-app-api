45aab57354310aeb8b3a4ca69e8e4b8e5a3f2fa5
Jacob <jacobboisseau@gmail.com>
2018-08-07 12:04:08 -0400

    adding-recurring-to-new-employees: create recurring expense types on creation

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,6 +1,9 @@
 const Crud = require('./crudRoutes');
 const databaseModify = require('../js/databaseModify');
 const employeeDynamo = new databaseModify('employees');
+const moment = require('moment');
+const _ = require('lodash');
+const uuid = require('uuid/v4');
 
 class EmployeeRoutes extends Crud {
   constructor() {
@@ -13,20 +16,23 @@ class EmployeeRoutes extends Crud {
       middleName = ' ';
     }
 
-    return new Promise(resolve => {
-      resolve({
-        id: uuid,
-        firstName,
-        middleName,
-        lastName,
-        empId,
-        hireDate,
-        expenseTypes,
-        email,
-        employeeRole,
-        isActive: true
+    let expense = {
+      id: uuid,
+      firstName,
+      middleName,
+      lastName,
+      empId,
+      hireDate,
+      expenseTypes,
+      email,
+      employeeRole,
+      isActive: true
+    };
+
+    return this._createRecurringExpenses(uuid, hireDate)
+      .then(() => {
+        return expense;
       });
-    });
   }
 
   _update(id, { firstName, middleName, lastName, empId, hireDate, expenseTypes, email, employeeRole, isActive }) {
@@ -55,6 +61,52 @@ class EmployeeRoutes extends Crud {
       .catch(err => {
         throw err;
       });
+  }
+
+  async _createRecurringExpenses(userId, hireDate) {
+    const budgetDynamo = new databaseModify('budgets');
+    const expenseTypeDynamo = new databaseModify('expense-types');
+
+    let dates = this._getBudgetDates(hireDate);
+    let expenseTypeList;
+    //get all recurring expenseTypes
+    try{
+      expenseTypeList = await expenseTypeDynamo.getAllEntriesInDB();
+    }
+    catch(err){ throw err; }
+    expenseTypeList = _.filter(expenseTypeList, exp => exp.recurringFlag );
+    return _.forEach(expenseTypeList, (recurringExpenseType) => {
+
+      let newBudget = {
+        id: uuid(),
+        expenseTypeId: recurringExpenseType.expenseTypeId,
+        userId: userId,
+        reimbursedAmount: 0,
+        pendingAmount: 0,
+        fiscalStartDate: dates.startDate.format('YYYY-MM-DD'),
+        fiscalEndDate: dates.endDate.format('YYYY-MM-DD')
+      };
+      return budgetDynamo.addToDB(newBudget).then(() => {
+        return; //tell forEach to continue looping
+      });
+    });
+
+    // return Promise.resolve();
+    //for each recurring expense type in the list
+    //create a budget using the userId and the expenseTypeId
+  }
+
+  _getBudgetDates(hireDate) {
+    let currentYear = moment().year();
+    let anniversaryMonth = moment(hireDate, 'YYYY-MM-DD').month(); // form 0-11
+    let anniversaryDay = moment(hireDate, 'YYYY-MM-DD').date();  // from 1 to 31
+    let startDate = moment([currentYear, anniversaryMonth, anniversaryDay]);
+    let endDate = moment([currentYear + 1, anniversaryMonth, anniversaryDay - 1 ]);
+
+    return {
+      startDate,
+      endDate
+    };
   }
 }
 
