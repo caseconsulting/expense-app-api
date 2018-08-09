d0f67379a4e8192b77c851d199a8d4d2c7cffe3f
Fahad <fahada95@vt.edu>
2018-08-09 09:31:50 -0400

    cloud-formation-part-two: updated cloudFront distro to have custom error responses for 403 and 404

---
+++ something.js
@@ -0,0 +1,59 @@
+const _ = require('lodash');
+const databaseModify = require('js/databaseModify');
+const budgetDynamo = new databaseModify('budgets');
+const expenseTypeDynamo = new databaseModify('expense-types');
+const uuid = require('uuid/v4');
+const moment = require('moment');
+
+export async function something() {
+  let budgets, expenseTypes;
+  try {
+    budgets = await budgetDynamo.getAllEntriesInDB(); //get all budgets
+    expenseTypes = await expenseTypeDynamo.getAllEntriesInDB(); //get all expensetypes
+  } catch (err) {
+    throw err;
+  }
+
+  _.forEach(budgets, oldBudget => {
+    //budget anniversary date is today
+    if (moment(oldBudget.fiscalEndDate).isSame(moment().format('YYYY-MM-DD'))) {
+      let expenseType = _getExpenseType(expenseTypes, oldBudget.expenseTypeId);
+      if (expenseType.recurringFlag) {
+        //filter by the ones that are recurring
+        let newBudget = _makeNewBudget(oldBudget, expenseType);
+        return budgetDynamo.addToDB(newBudget);
+      }
+    }
+  });
+}
+
+function _getExpenseType(expenseTypes, expenseTypeId) {
+  return _.find(expenseTypes, expenseType => {
+    return expenseType.id === expenseTypeId;
+  });
+}
+
+function _makeNewBudget(oldBudget, expenseType) {
+  let newBudget = {
+    id: uuid(),
+    expenseTypeId: oldBudget.expenseTypeId,
+    userId: oldBudget.userId,
+    reimbursedAmount: 0,
+    pendingAmount: 0,
+    fiscalStartDate: moment(oldBudget.fiscalStartDate)
+      .add(1, 'years')
+      .format('YYYY-MM-DD'), //increment the budgets fiscal start day by one year
+    fiscalEndDate: moment(oldBudget.fiscalEndDate)
+      .add(1, 'years')
+      .format('YYYY-MM-DD') //increment the budgets fiscal end day by one year
+  };
+
+  if (oldBudget.pendingAmount > expenseType.budget) {
+    newBudget.pendingAmount = oldBudget.pendingAmount - expenseType.budget;
+  }
+  if (oldBudget.reimbursedAmount > expenseType.budget) {
+    newBudget.reimbursedAmount = oldBudget.reimbursedAmount - expenseType.budget;
+  }
+
+  return newBudget;
+}
