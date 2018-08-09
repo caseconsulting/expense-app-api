1b3537f9e38f39daa06f4efda45edbe2296b8773
Fahad <fahada95@vt.edu>
2018-08-09 09:00:19 -0400

    nightly-script: updated nightly script

--- nightly-script.js
+++ nightly-script.js
@@ -1,20 +1,58 @@
-// let start, end;
-// if (expenseType.recurringFlag) {
-//   let hireDate = moment(employee.hireDate);
-//   let hireYear = hireDate.year();
-//   let currentYear = moment().year();
-//   let yearDiff = currentYear - hireYear;
-//   let fiscalStartDate = hireDate.add(yearDiff, 'years');
-//   let fiscalEndDate = fiscalStartDate.add(1, 'years');
-//
-//   fiscalEndDate = fiscalEndDate.subtract(1, 'days');
-//   start = moment(fiscalStartDate).format('MM-DD-YYYY');
-//   end = moment(fiscalEndDate).format('MM-DD-YYYY');
-// } else {
-//   start = expenseType.fiscalStartDate;
-//   end = expenseType.fiscalEndDate;
-// }
-// return {
-//   startDate: start,
-//   endDate: end
-// };
+const _ = require('lodash');
+const databaseModify = require('js/databaseModify');
+const budgetDynamo = new databaseModify('budgets');
+const expenseTypeDynamo = new databaseModify('expense-types');
+const uuid = require('uuid/v4');
+const moment = require('moment');
+
+export async function something() {
+  try {
+    budgets = await budgetDynamo.getAllEntriesInDB(); //get all budgets
+    expenseTypes = await expenseTypeDynamo.getAllEntriesInDB(); //get all expensetypes
+  } catch (err) {
+    throw err;
+  }
+
+  _.forEach(budgets, oldBudget => {
+    //budget anniversary date is today
+    if (moment(oldBudget.fiscalEndDate).isSame(moment().format('YYYY-MM-DD'))) {
+      let expenseType = _getExpenseType(expenseTypes, oldBudget.expenseTypeId);
+      if (expenseType.recurringFlag) {
+        //filter by the ones that are recurring
+        let newBudget = _makeNewBudget(oldBudget, expenseType);
+        return budgetDynamo.addToDB(newBudget);
+      }
+    }
+  });
+}
+
+function _getExpenseType(expenseTypes, expenseTypeId) {
+  return _.find(expenseTypes, expenseType => {
+    return expenseType.id === expenseTypeId;
+  });
+}
+
+function _makeNewBudget(oldBudget, expenseType) {
+  let newBudget = {
+    id: uuid(),
+    expenseTypeId: oldBudget.expenseTypeId,
+    userId: oldBudget.userId,
+    reimbursedAmount: 0,
+    pendingAmount: 0,
+    fiscalStartDate: moment(oldBudget.fiscalStartDate)
+      .add(1, 'years')
+      .format('YYYY-MM-DD'), //increment the budgets fiscal start day by one year
+    fiscalEndDate: moment(oldBudget.fiscalEndDate)
+      .add(1, 'years')
+      .format('YYYY-MM-DD') //increment the budgets fiscal end day by one year
+  };
+
+  if (oldBudget.pendingAmount > expenseType.budget) {
+    newBudget.pendingAmount = oldBudget.pendingAmount - expenseType.budget;
+  }
+  if (oldBudget.reimbursedAmount > expenseType.budget) {
+    newBudget.reimbursedAmount = oldBudget.reimbursedAmount - expenseType.budget;
+  }
+
+  return newBudget;
+}
