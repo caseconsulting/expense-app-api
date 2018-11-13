4486b118a6c7092cfa86bcf27a2b22651fe5a946
Michael Frank <mfrankwork@gmail.com>
2018-11-12 21:12:53 -0500

    333-new-budgets: Fix _getBudgetDates in routes/expenseRoutes.js

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -179,8 +179,10 @@ class ExpenseRoutes extends Crud {
     let currentYear = moment().year();
     let anniversaryMonth = moment(hireDate, 'YYYY-MM-DD').month(); // form 0-11
     let anniversaryDay = moment(hireDate, 'YYYY-MM-DD').date(); // from 1 to 31
-    let startDate = moment([currentYear, anniversaryMonth, anniversaryDay]);
-    let endDate = moment([currentYear + 1, anniversaryMonth, anniversaryDay - 1]);
+    const anniversaryComparisonDate = moment([currentYear, anniversaryMonth, anniversaryDay]);
+    let startYear = anniversaryComparisonDate.isSameOrBefore(moment(), 'day') ? currentYear : currentYear - 1;
+    let startDate = moment([startYear, anniversaryMonth, anniversaryDay]);
+    let endDate = moment([startYear + 1, anniversaryMonth, anniversaryDay - 1]);
 
     return {
       startDate,
