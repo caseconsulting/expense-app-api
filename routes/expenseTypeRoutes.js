60a0baaa421948a0d13e176637b943c5575c891b
Sam Peake <sampeake@Sams-MacBook-Pro.local>
2019-01-09 10:27:50 -0500

    345-Validation-error-message-is-partially-hidden: disallowed expense types with start dates after end dates

--- routes/expenseTypeRoutes.js
+++ routes/expenseTypeRoutes.js
@@ -1,6 +1,10 @@
 const Crud = require('./crudRoutes');
 const databaseModify = require('../js/databaseModify');
 const expenseTypesDyanmo = new databaseModify('expense-types');
+const Moment = require('moment');
+const MomentRange = require('moment-range');
+const IsoFormat = 'YYYY-MM-DD';
+const moment = MomentRange.extendMoment(Moment);
 
 class ExpenseTypeRoutes extends Crud {
   constructor() {
@@ -8,6 +12,22 @@ class ExpenseTypeRoutes extends Crud {
     this.databaseModify = expenseTypesDyanmo;
   }
   _add(uuid, { budgetName, budget, odFlag, description,startDate, endDate, recurringFlag }) {
+    let dateCheck = recurringFlag || moment(startDate, IsoFormat).isSameOrBefore(endDate, IsoFormat);
+    if(!dateCheck) {
+      let err = {
+        code: 403,
+        message: 'Invalid Start Date and End Date.'
+      };
+      throw err;
+    }
+    let validFieldsCheck = budget > 0 && !!description && !!budgetName;
+    if(!validFieldsCheck) {
+      let err = {
+        code: 403,
+        message: 'There is a missing field in either the budget, description, or name.'
+      };
+      throw err;
+    }
     return new Promise(resolve => {
       if (odFlag === undefined) {
         odFlag = false;
@@ -26,6 +46,22 @@ class ExpenseTypeRoutes extends Crud {
   }
 
   _update(id, { budgetName, budget, odFlag, description,startDate, endDate, recurringFlag }) {
+    let dateCheck = recurringFlag || moment(startDate, IsoFormat).isSameOrBefore(endDate, IsoFormat);
+    if(!dateCheck) {
+      let err = {
+        code: 403,
+        message: 'Invalid Start Date and End Date.'
+      };
+      throw err;
+    }
+    let validFieldsCheck = budget > 0 && !!description && !!budgetName;
+    if(!validFieldsCheck) {
+      let err = {
+        code: 403,
+        message: 'There is a missing field in either the budget, description, or name.'
+      };
+      throw err;
+    }
     return this.databaseModify
       .findObjectInDB(id)
       .then(() => {
@@ -44,5 +80,6 @@ class ExpenseTypeRoutes extends Crud {
         throw err;
       });
   }
+
 }
 module.exports = ExpenseTypeRoutes;
