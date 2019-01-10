a7a90c5380554faeaa72eb612dc155e8a5c9d1ac
Sam Peake <sampeake@Sams-MacBook-Pro.local>
2019-01-10 10:15:50 -0500

    322-Dates-for-expense-types-need-validation: made more changes to prevent invalid dates

--- routes/expenseTypeRoutes.js
+++ routes/expenseTypeRoutes.js
@@ -1,6 +1,7 @@
 const Crud = require('./crudRoutes');
 const databaseModify = require('../js/databaseModify');
 const expenseTypesDyanmo = new databaseModify('expense-types');
+const uuid = require('uuid/v4');
 const Moment = require('moment');
 const MomentRange = require('moment-range');
 const IsoFormat = 'YYYY-MM-DD';
@@ -12,74 +13,66 @@ class ExpenseTypeRoutes extends Crud {
     this.databaseModify = expenseTypesDyanmo;
   }
   _add(uuid, { budgetName, budget, odFlag, description,startDate, endDate, recurringFlag }) {
-    let dateCheck = recurringFlag || moment(startDate, IsoFormat).isSameOrBefore(endDate, IsoFormat);
-    if(!dateCheck) {
-      let err = {
-        code: 403,
-        message: 'Invalid Start Date and End Date.'
-      };
-      throw err;
-    }
-    let validFieldsCheck = budget > 0 && !!description && !!budgetName;
-    if(!validFieldsCheck) {
-      let err = {
-        code: 403,
-        message: 'There is a missing field in either the budget, description, or name.'
-      };
-      throw err;
-    }
-    return new Promise(resolve => {
-      if (odFlag === undefined) {
-        odFlag = false;
-      }
-      resolve({
-        id: uuid,
-        budgetName,
-        budget,
-        odFlag,
-        description,
-        startDate,
-        endDate,
-        recurringFlag
-      });
-    });
-  }
-
-  _update(id, { budgetName, budget, odFlag, description,startDate, endDate, recurringFlag }) {
-    let dateCheck = recurringFlag || moment(startDate, IsoFormat).isSameOrBefore(endDate, IsoFormat);
-    if(!dateCheck) {
-      let err = {
-        code: 403,
-        message: 'Invalid Start Date and End Date.'
-      };
-      throw err;
-    }
-    let validFieldsCheck = budget > 0 && !!description && !!budgetName;
-    if(!validFieldsCheck) {
-      let err = {
-        code: 403,
-        message: 'There is a missing field in either the budget, description, or name.'
-      };
-      throw err;
-    }
-    return this.databaseModify
-      .findObjectInDB(id)
-      .then(() => {
-        return {
-          id,
-          budgetName,
-          budget,
-          odFlag,
-          description,
-          startDate,
-          endDate,
-          recurringFlag
-        };
-      })
+    let expenseType = {
+      id: uuid,
+      budgetName: budgetName,
+      budget: parseFloat(budget),
+      odFlag: odFlag,
+      description: description,
+      startDate: startDate,
+      endDate: endDate,
+      recurringFlag: recurringFlag
+    };
+    return this.checkValidity(expenseType)
+      .then(() => expenseTypesDyanmo.addToDB(expenseType))
       .catch(err => {
         throw err;
       });
   }
 
+  _update(id, { budgetName, budget, odFlag, description,startDate, endDate, recurringFlag }) {
+    let expenseType = {
+      id: uuid,
+      budgetName: budgetName,
+      budget: parseFloat(budget),
+      odFlag: odFlag,
+      description: description,
+      startDate: startDate,
+      endDate: endDate,
+      recurringFlag: recurringFlag
+    };
+    return this.checkValidity(expenseType)
+      .then(() => expenseTypesDyanmo.updateEntryInDB(expenseType))
+      .catch(err => {
+        throw err;
+      });
+  }
+  checkValidity(expenseType) {
+    let idCheck = !!expenseType.id;
+    let budgetNameCheck = !!expenseType.budgetName;
+    let budgetCheck =  expenseType.budget > 0;
+    let descriptionCheck = !!expenseType.description;
+    let recurringBool = expenseType !== undefined;
+    let startDateCheck = recurringBool || !!expenseType.startDate;
+    let endDateCheck = recurringBool || !!expenseType.endDate;
+    let datesCheck = moment(expenseType.startDate, IsoFormat).isBefore(expenseType.endDate, IsoFormat);
+    let fieldsCheck = idCheck && budgetNameCheck && budgetCheck && descriptionCheck;
+    let validDates = startDateCheck && endDateCheck && datesCheck;
+    let valid = fieldsCheck && validDates;
+    let errMessage = '';
+    if(!valid) {
+      if(!fieldsCheck) {
+        errMessage.append('One of the fields is empty.\n');
+      }
+      if(!validDates) {
+        errMessage.append('The dates are not valid');
+      }
+    }
+    let error = {
+      code: 403,
+      message: errMessage
+    };
+    return valid ? Promise.resolve() : Promise.reject(error);
+  }
 }
 module.exports = ExpenseTypeRoutes;
