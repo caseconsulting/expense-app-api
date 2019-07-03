53d7bc223e0b8e8e47066276a11d95b03e6bb095
Jacob Boisseau <jacobboisseau@gmail.com>
2019-07-03 08:58:40 -0400

    employee routes _add test working
    Signed-off-by: Jacob Boisseau <jacobboisseau@gmail.com>

--- spec/routes/employeeRoutes.spec.js
+++ spec/routes/employeeRoutes.spec.js
@@ -1,5 +1,6 @@
 const EmployeeRoutes = require('../../routes/employeeRoutes');
 const Employee = require('../../models/employee');
+<<<<<<< HEAD
 
 describe('employeeRoutes', () => {
   let expenseDynamo, budgetDynamo, expenseTypeDynamo, employeeDynamo, employeeRoutes;
@@ -30,6 +31,7 @@ describe('employeeRoutes', () => {
     employeeRoutes.expenseDynamo = expenseDynamo;
     spyOn(employeeRoutes, 'getUUID').and.returnValue(uuid);
   });
+
 
   describe('_add', () => {
     let expectedEmployee, data;
