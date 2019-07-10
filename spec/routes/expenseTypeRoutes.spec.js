3317bfdb0157b9bc2b493c7d5ba0c1abbef1a17c
Jacob Boisseau <jacobboisseau@gmail.com>
2019-07-10 15:31:32 -0400

    removed xdescribe in expense type routes

--- spec/routes/expenseTypeRoutes.spec.js
+++ spec/routes/expenseTypeRoutes.spec.js
@@ -1,7 +1,7 @@
 const ExpenseTypeRoutes = require('../../routes/expenseTypeRoutes');
 const ExpenseType = require('../../models/expenseType');
 
-xdescribe('expenseTypeRoutes', () => {
+describe('expenseTypeRoutes', () => {
   const id = 'id';
   const budgetName = 'budgetName';
   const budget = 1000;
