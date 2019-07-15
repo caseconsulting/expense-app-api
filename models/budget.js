2f84f91c0d413af7d403b7d384ef71380b8c7b1e
Robert Schofield <rschofield@consultwithcase.com>
2019-07-14 22:02:47 -0400

    425-budget-model-for-api: add budget model for api

---
+++ models/budget.js
@@ -0,0 +1,26 @@
+/**
+ * Budget model
+ *
+ * Fields:
+ * - id
+ * - expenseTypeId
+ * - userId
+ * - reimbursedAmount
+ * - pendingAmount
+ * - fiscalStartDate
+ * - fiscalEndDate
+ */
+
+class Budget {
+  constructor(data) {
+    this.id = data.id;
+    this.expenseTypeId = data.expenseTypeId;
+    this.userId = data.userId;
+    this.reimbursedAmount = Number(data.reimbursedAmount);
+    this.pendingAmount = Number(data.pendingAmount);
+    this.fiscalStartDate = data.fiscalStartDate;
+    this.fiscalEndDate = data.fiscalEndDate;
+  }
+}
+
+module.exports = Budget;
