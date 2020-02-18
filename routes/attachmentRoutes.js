d6d94107244c4c10fadec3be570e3aa2dad7436b
austinjl <alam@consultwithcase.com>
2020-02-18 08:27:24 -0500

    803-As-a-developer,-when-an-expense-is-deleted-I-want-the-receipt-to-be-deleted-from-the-S3-bucket-also: Make delete request to S3 for attachement

--- routes/attachmentRoutes.js
+++ routes/attachmentRoutes.js
@@ -105,8 +105,9 @@ class Attachment {
   constructor() {
     this.expenseData = expenseDynamo;
     this._router = express.Router();
-    this._router.post('/:userId/:expenseId', checkJwt, getUserInfo, this.afterUpload.bind(this));
+    this._router.post('/:userId/:expenseId', checkJwt, getUserInfo, this.uploadAttachmentToS3.bind(this));
     this.router.get('/:userId/:expenseId', checkJwt, getUserInfo, this.getAttachmentFromS3.bind(this));
+    this.router.delete('/:userId/:expenseId/:receipt', checkJwt, getUserInfo, this.deleteAttachmentFromS3.bind(this));
   }
 
   get router() {
@@ -114,48 +115,45 @@ class Attachment {
   }
 
   /**
-   * Post-processing after file upload
+   * Uploads an object to S3.
    */
-  afterUpload(req, res) {
-
+  uploadAttachmentToS3(req, res) {
     upload(req, res, (err) => {
       if (err) {
         // if there is an error from validating the file
         console.warn(
           `[${moment().format()}]`,
           '>>> Failed to upload file',
-          '| Processing handled by function attachmentRoutes.afterUpload'
+          '| Processing handled by function attachmentRoutes.uploadAttachmentToS3'
         );
-
         let error = {
           code: 403,
           message:
             `${err}`
         };
-
         return res.send(error);
-
       } else {
         // successfully validated file
         console.warn(
           `[${moment().format()}]`,
           `>>> Successfully uploaded attachment ${req.file.originalname}`,
           `with file key ${req.file.key} to S3 bucket ${req.file.bucket}`,
-          '| Processing handled by function attachmentRoutes.afterUpload'
+          '| Processing handled by function attachmentRoutes.uploadAttachmentToS3'
         );
-
         res.send('Successfully uploaded file:' + req.file.key);
       }
     });
   }
 
+  /**
+   * Gets an object from S3.
+   */
   async getAttachmentFromS3(req, res) {
     console.warn(
       `[${moment().format()}]`,
       `>>> Getting attachment for expense ${req.params.expenseId}`,
       '| Processing handled by function attachmentRoutes.getAttachmentFromS3'
     );
-
     let expense = await this.expenseData.findObjectInDB(req.params.expenseId);
     let fileExt = expense.receipt;
     let filePath = `${req.params.userId}/${req.params.expenseId}/${fileExt}`;
@@ -163,6 +161,46 @@ class Attachment {
     s3.getSignedUrl('getObject', params, (err, data) => {
       if (err) throw err;
       else res.status(200).send(data);
+    });
+  }
+
+  /**
+   * Deletes an object from S3.
+   */
+  async deleteAttachmentFromS3(req, res) {
+    console.warn(
+      `[${moment().format()}]`,
+      `>>> Attempting to delete attachment for expense ${req.params.expenseId}`,
+      '| Processing handled by function attachmentRoutes.deleteAttachmentFromS3'
+    );
+    // set up params
+    let fileExt = req.params.receipt;
+    let filePath = `${req.params.userId}/${req.params.expenseId}/${fileExt}`;
+    let params = { Bucket: BUCKET, Key: filePath};
+    // make delete call to s3
+    s3.deleteObject(params, (err, data) => {
+      if (err) {
+        // if there is an error deleting the file
+        console.warn(
+          `[${moment().format()}]`,
+          `>>> Failed to delete attachment for expense ${req.params.expenseId} from S3 ${filePath}`,
+          '| Processing handled by function attachmentRoutes.deleteAttachmentFromS3'
+        );
+        let error = {
+          code: 403,
+          message:
+            `${err.message}`
+        };
+        return res.send(error);
+      } else {
+        // successfully deleted the file
+        console.warn(
+          `[${moment().format()}]`,
+          `>>> Successfully deleted attachment for expense ${req.params.expenseId} from S3 ${filePath}`,
+          '| Processing handled by function attachmentRoutes.deleteAttachmentFromS3'
+        );
+        res.status(200).send(data);
+      }
     });
   }
 }
