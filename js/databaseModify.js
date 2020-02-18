69bcf5aa0b031825a574bedd4ae44baefac250e0
austinjl <alam@consultwithcase.com>
2020-02-18 11:07:34 -0500

    868-As-a-developer,-I-want-improved-api-unit-tests: Alphabetize method names

--- js/databaseModify.js
+++ js/databaseModify.js
@@ -19,26 +19,97 @@ class databaseModify {
     });
   }
 
-  /**
-   * returns the value if it exists
-   */
-  findObjectInDB(primaryKey) {
-    return this.readFromDB(primaryKey)
-      .then(function(data) {
-        if (_.first(data)) {
-          return _.first(data);
+  _buildBudgetUpdateParams(objToUpdate) {
+    return _.assign(
+      {
+        TableName: `${STAGE}-budgets`,
+        Key: {
+          id: objToUpdate.id
+        },
+        ReturnValues: 'ALL_NEW'
+      },
+      this._buildExpression(objToUpdate)
+    );
+  }
+
+  _buildEmployeeUpdateParams(objToUpdate) {
+    return _.assign(
+      {
+        TableName: `${STAGE}-employees`,
+        Key: {
+          id: objToUpdate.id
+        },
+        ReturnValues: 'ALL_NEW'
+      },
+      this._buildExpression(objToUpdate)
+    );
+  }
+
+  _buildExpenseTypeUpdateParams(objToUpdate) {
+    return _.assign(
+      {
+        TableName: `${STAGE}-expense-types`,
+        Key: {
+          id: objToUpdate.id
+        },
+        ReturnValues: 'ALL_NEW'
+      },
+      this._buildExpression(objToUpdate)
+    );
+  }
+
+  _buildExpenseUpdateParams(objToUpdate) {
+    return _.assign(
+      {
+        TableName: `${STAGE}-expenses`,
+        Key: {
+          id: objToUpdate.id
+        },
+        ReturnValues: 'ALL_NEW'
+      },
+      this._buildExpression(objToUpdate)
+    );
+  }
+
+  _buildExpression(data) {
+    const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let ExpressionAttributeValues = {};
+    let UpdateExpression = 'set ';
+    let ExpressionAttributeNames = {};
+    const attributes = _.keys(_.omit(data, ['id', 'category']));
+
+    _.each(attributes, (attribute, index) => {
+      const value = _.get(data, attribute);
+      if (value || value === 0) {
+        let expressionAttribute = `:${alpha[index]}`;
+        ExpressionAttributeValues[expressionAttribute] = value;
+
+        if (attribute === 'url') { // what is this case for? - austin
+          UpdateExpression += `#url = ${expressionAttribute},`;
+          _.assign(ExpressionAttributeNames, { '#url': 'url' });
         } else {
-          let err = {
-            code: 404,
-            message: 'Entry not found in database'
-          };
-          throw err;
+          UpdateExpression += `${attribute} = ${expressionAttribute},`;
         }
-      })
-      .catch(function(err) {
-        console.error(err);
-        throw err; //Throw error and handle properly in crudRoutes
-      });
+      }
+    });
+    UpdateExpression = `${_.trimEnd(UpdateExpression, ',')}`;
+    return !_.isEmpty(ExpressionAttributeNames)
+      ? { ExpressionAttributeValues, UpdateExpression, ExpressionAttributeNames }
+      : { ExpressionAttributeValues, UpdateExpression };
+  }
+
+  _buildTrainingUrlUpdateParams(objToUpdate) {
+    return _.assign(
+      {
+        TableName: `${STAGE}-training-urls`,
+        Key: {
+          id: objToUpdate.id,
+          category: objToUpdate.category
+        },
+        ReturnValues: 'ALL_NEW'
+      },
+      this._buildExpression(objToUpdate)
+    );
   }
 
   /**
@@ -68,6 +139,134 @@ class databaseModify {
       };
       return Promise.reject(err);
     }
+  }
+
+  buildExpressionAttributeValues(objToUpdate) {
+    return _.pickBy(
+      {
+        ':pd': objToUpdate.purchaseDate,
+        ':rd': objToUpdate.reimbursedDate,
+        ':c': objToUpdate.cost,
+        ':d': objToUpdate.description,
+        ':n': objToUpdate.note,
+        ':r': objToUpdate.receipt,
+        ':eti': objToUpdate.expenseTypeId,
+        ':ui': objToUpdate.userId,
+        ':cat': objToUpdate.createdAt,
+        ':rurl': objToUpdate.url,
+        ':cate': objToUpdate.categories
+      },
+      _.identity
+    );
+  }
+
+  /**
+   * Builds the parameters for update depending on the this.tableName
+   * @return the parameters for update
+   */
+  buildUpdateParams(objToUpdate) {
+    switch (this.tableName) {
+      case `${STAGE}-expenses`:
+        return this._buildExpenseUpdateParams(objToUpdate);
+      case `${STAGE}-employees`:
+        return this._buildEmployeeUpdateParams(objToUpdate);
+      case `${STAGE}-expense-types`:
+        return this._buildExpenseTypeUpdateParams(objToUpdate);
+      case `${STAGE}-budgets`:
+        return this._buildBudgetUpdateParams(objToUpdate);
+      case `${STAGE}-training-urls`:
+        return this._buildTrainingUrlUpdateParams(objToUpdate);
+    }
+  }
+
+  /**
+   * returns the value if it exists
+   */
+  findObjectInDB(primaryKey) {
+    return this.readFromDB(primaryKey)
+      .then(function(data) {
+        if (_.first(data)) {
+          return _.first(data);
+        } else {
+          let err = {
+            code: 404,
+            message: 'Entry not found in database'
+          };
+          throw err;
+        }
+      })
+      .catch(function(err) {
+        console.error(err);
+        throw err; //Throw error and handle properly in crudRoutes
+      });
+  }
+
+  getAllEntriesInDB() {
+    var params = {
+      TableName: this.tableName
+    };
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    return documentClient
+      .scan(params)
+      .promise()
+      .then(function(data) {
+        return _.sortBy(data.Items, ['lastName', 'middleName', 'firstName', 'budgetName', 'purchaseDate']);
+      })
+      .catch(function(err) {
+        console.error(err);
+        throw err;
+      });
+  }
+
+  querySecondaryIndexInDB(secondaryIndex, queryKey, queryParam) {
+    const params = {
+      TableName: this.tableName,
+      IndexName: secondaryIndex,
+      ExpressionAttributeValues: {
+        ':queryKey': queryParam
+      },
+      KeyConditionExpression: `${queryKey} = :queryKey`
+    };
+
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    return documentClient
+      .query(params)
+      .promise()
+      .then(data => {
+        return data.Items;
+      })
+      .catch(err => {
+        console.error(err);
+        throw err;
+      });
+  }
+
+  queryWithTwoIndexesInDB(userId, expenseTypeId) {
+    const params = {
+      TableName: this.tableName,
+      IndexName: 'userId-expenseTypeId-index',
+      ExpressionAttributeValues: {
+        ':expenseTypeId': expenseTypeId,
+        ':userId': userId
+      },
+      KeyConditionExpression: 'expenseTypeId = :expenseTypeId and userId = :userId'
+    };
+
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    return documentClient
+      .query(params)
+      .promise()
+      .then(data => {
+        if (!_.isEmpty(data.Items)) {
+          return data.Items;
+        } else {
+          return null;
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        throw err;
+      });
   }
 
   readFromDB(passedID) {
@@ -133,56 +332,7 @@ class databaseModify {
       });
   }
 
-  queryWithTwoIndexesInDB(userId, expenseTypeId) {
-    const params = {
-      TableName: this.tableName,
-      IndexName: 'userId-expenseTypeId-index',
-      ExpressionAttributeValues: {
-        ':expenseTypeId': expenseTypeId,
-        ':userId': userId
-      },
-      KeyConditionExpression: 'expenseTypeId = :expenseTypeId and userId = :userId'
-    };
 
-    const documentClient = new AWS.DynamoDB.DocumentClient();
-    return documentClient
-      .query(params)
-      .promise()
-      .then(data => {
-        if (!_.isEmpty(data.Items)) {
-          return data.Items;
-        } else {
-          return null;
-        }
-      })
-      .catch(err => {
-        console.error(err);
-        throw err;
-      });
-  }
-
-  querySecondaryIndexInDB(secondaryIndex, queryKey, queryParam) {
-    const params = {
-      TableName: this.tableName,
-      IndexName: secondaryIndex,
-      ExpressionAttributeValues: {
-        ':queryKey': queryParam
-      },
-      KeyConditionExpression: `${queryKey} = :queryKey`
-    };
-
-    const documentClient = new AWS.DynamoDB.DocumentClient();
-    return documentClient
-      .query(params)
-      .promise()
-      .then(data => {
-        return data.Items;
-      })
-      .catch(err => {
-        console.error(err);
-        throw err;
-      });
-  }
 
   /**
    * Removes the object from the database according to its index key
@@ -219,154 +369,6 @@ class databaseModify {
         console.error(err);
         throw err;
       });
-  }
-
-  getAllEntriesInDB() {
-    var params = {
-      TableName: this.tableName
-    };
-    const documentClient = new AWS.DynamoDB.DocumentClient();
-    return documentClient
-      .scan(params)
-      .promise()
-      .then(function(data) {
-        return _.sortBy(data.Items, ['lastName', 'middleName', 'firstName', 'budgetName', 'purchaseDate']);
-      })
-      .catch(function(err) {
-        console.error(err);
-        throw err;
-      });
-  }
-
-  buildExpressionAttributeValues(objToUpdate) {
-    return _.pickBy(
-      {
-        ':pd': objToUpdate.purchaseDate,
-        ':rd': objToUpdate.reimbursedDate,
-        ':c': objToUpdate.cost,
-        ':d': objToUpdate.description,
-        ':n': objToUpdate.note,
-        ':r': objToUpdate.receipt,
-        ':eti': objToUpdate.expenseTypeId,
-        ':ui': objToUpdate.userId,
-        ':cat': objToUpdate.createdAt,
-        ':rurl': objToUpdate.url,
-        ':cate': objToUpdate.categories
-      },
-      _.identity
-    );
-  }
-
-  _buildExpression(data) {
-    const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    let ExpressionAttributeValues = {};
-    let UpdateExpression = 'set ';
-    let ExpressionAttributeNames = {};
-    const attributes = _.keys(_.omit(data, ['id', 'category']));
-
-    _.each(attributes, (attribute, index) => {
-      const value = _.get(data, attribute);
-      if (value || value === 0) {
-        let expressionAttribute = `:${alpha[index]}`;
-        ExpressionAttributeValues[expressionAttribute] = value;
-
-        if (attribute === 'url') { // what is this case for? - austin
-          UpdateExpression += `#url = ${expressionAttribute},`;
-          _.assign(ExpressionAttributeNames, { '#url': 'url' });
-        } else {
-          UpdateExpression += `${attribute} = ${expressionAttribute},`;
-        }
-      }
-    });
-    UpdateExpression = `${_.trimEnd(UpdateExpression, ',')}`;
-    return !_.isEmpty(ExpressionAttributeNames)
-      ? { ExpressionAttributeValues, UpdateExpression, ExpressionAttributeNames }
-      : { ExpressionAttributeValues, UpdateExpression };
-  }
-
-  _buildExpenseUpdateParams(objToUpdate) {
-    return _.assign(
-      {
-        TableName: `${STAGE}-expenses`,
-        Key: {
-          id: objToUpdate.id
-        },
-        ReturnValues: 'ALL_NEW'
-      },
-      this._buildExpression(objToUpdate)
-    );
-  }
-
-  _buildEmployeeUpdateParams(objToUpdate) {
-    return _.assign(
-      {
-        TableName: `${STAGE}-employees`,
-        Key: {
-          id: objToUpdate.id
-        },
-        ReturnValues: 'ALL_NEW'
-      },
-      this._buildExpression(objToUpdate)
-    );
-  }
-
-  _buildExpenseTypeUpdateParams(objToUpdate) {
-    return _.assign(
-      {
-        TableName: `${STAGE}-expense-types`,
-        Key: {
-          id: objToUpdate.id
-        },
-        ReturnValues: 'ALL_NEW'
-      },
-      this._buildExpression(objToUpdate)
-    );
-  }
-
-  _buildBudgetUpdateParams(objToUpdate) {
-    return _.assign(
-      {
-        TableName: `${STAGE}-budgets`,
-        Key: {
-          id: objToUpdate.id
-        },
-        ReturnValues: 'ALL_NEW'
-      },
-      this._buildExpression(objToUpdate)
-    );
-  }
-
-  _buildTrainingUrlUpdateParams(objToUpdate) {
-    return _.assign(
-      {
-        TableName: `${STAGE}-training-urls`,
-        Key: {
-          id: objToUpdate.id,
-          category: objToUpdate.category
-        },
-        ReturnValues: 'ALL_NEW'
-      },
-      this._buildExpression(objToUpdate)
-    );
-  }
-
-  /**
-   * Builds the parameters for update depending on the this.tableName
-   * @return the parameters for update
-   */
-  buildUpdateParams(objToUpdate) {
-    switch (this.tableName) {
-      case `${STAGE}-expenses`:
-        return this._buildExpenseUpdateParams(objToUpdate);
-      case `${STAGE}-employees`:
-        return this._buildEmployeeUpdateParams(objToUpdate);
-      case `${STAGE}-expense-types`:
-        return this._buildExpenseTypeUpdateParams(objToUpdate);
-      case `${STAGE}-budgets`:
-        return this._buildBudgetUpdateParams(objToUpdate);
-      case `${STAGE}-training-urls`:
-        return this._buildTrainingUrlUpdateParams(objToUpdate);
-    }
   }
 }
 module.exports = databaseModify;
