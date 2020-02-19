687e4f3f942444ef02fe53344ac5a1fa1fbcbcd8
austinjl <alam@consultwithcase.com>
2020-02-19 13:59:37 -0500

    868-As-a-developer,-I-want-improved-api-unit-tests: Cover all tests for expenseTypeRoutes

--- routes/expenseTypeRoutes.js
+++ routes/expenseTypeRoutes.js
@@ -17,6 +17,61 @@ class ExpenseTypeRoutes extends Crud {
     this.expenseData = expenseDynamo;
   }
 
+  _add(id, data) {
+    console.warn(
+      `[${moment().format()}]`,
+      `>>> Attempting to add expense type ${id}`,
+      '| Processing handled by function expenseTypeRoutes._add'
+    );
+
+    let expenseType = new ExpenseType(data);
+    expenseType.id = id;
+
+    return this._checkFields(expenseType)
+      .then(() => this._checkDates(expenseType.startDate, expenseType.endDate, expenseType.recurringFlag))
+      .then(() => this.expenseTypeDynamo.addToDB(expenseType))
+      .catch(err => {
+        throw err;
+      });
+  }
+
+  _checkDates(startDate, endDate, recurringFlag) {
+    console.warn(
+      `[${moment().format()}]`,
+      'Validating expense type dates',
+      '| Processing handled by function expenseTypeRoutes._checkDates'
+    );
+
+    let valid = recurringFlag;
+    if (!valid && !!startDate && !!endDate) {
+      valid = moment(startDate, IsoFormat).isBefore(endDate, IsoFormat);
+    }
+    let err = {
+      code: 403,
+      message: 'The dates are invalid.'
+    };
+    return valid ? Promise.resolve() : Promise.reject(err);
+  }
+
+  _checkFields(expenseType) {
+    console.warn(
+      `[${moment().format()}]`,
+      `Validating expense type ${expenseType.id}`,
+      '| Processing handled by function expenseTypeRoutes._checkFields'
+    );
+
+    let idCheck = !!expenseType.id;
+    let budgetNameCheck = !!expenseType.budgetName;
+    let budgetCheck = expenseType.budget > 0;
+    let descriptionCheck = !!expenseType.description;
+    let valid = idCheck && budgetNameCheck && budgetCheck && descriptionCheck;
+    let err = {
+      code: 403,
+      message: 'One of the required fields is empty.'
+    };
+    return valid ? Promise.resolve() : Promise.reject(err);
+  }
+
   async _delete(id) {
     console.warn(
       `[${moment().format()}]`,
@@ -46,24 +101,6 @@ class ExpenseTypeRoutes extends Crud {
     }
   }
 
-  _add(id, data) {
-    console.warn(
-      `[${moment().format()}]`,
-      `>>> Attempting to add expense type ${id}`,
-      '| Processing handled by function expenseTypeRoutes._add'
-    );
-
-    let expenseType = new ExpenseType(data);
-    expenseType.id = id;
-
-    return this._checkFields(expenseType)
-      .then(() => this._checkDates(expenseType.startDate, expenseType.endDate, expenseType.recurringFlag))
-      .then(() => this.expenseTypeDynamo.addToDB(expenseType))
-      .catch(err => {
-        throw err;
-      });
-  }
-
   _update(id, data) {
     console.warn(
       `[${moment().format()}]`,
@@ -80,43 +117,6 @@ class ExpenseTypeRoutes extends Crud {
       .catch(err => {
         throw err;
       });
-  }
-
-  _checkFields(expenseType) {
-    console.warn(
-      `[${moment().format()}]`,
-      `Validating expense type ${expenseType.id}`,
-      '| Processing handled by function expenseTypeRoutes._checkFields'
-    );
-
-    let idCheck = !!expenseType.id;
-    let budgetNameCheck = !!expenseType.budgetName;
-    let budgetCheck = expenseType.budget > 0;
-    let descriptionCheck = !!expenseType.description;
-    let valid = idCheck && budgetNameCheck && budgetCheck && descriptionCheck;
-    let err = {
-      code: 403,
-      message: 'One of the required fields is empty.'
-    };
-    return valid ? Promise.resolve() : Promise.reject(err);
-  }
-
-  _checkDates(startDate, endDate, recurringFlag) {
-    console.warn(
-      `[${moment().format()}]`,
-      'Validating expense type dates',
-      '| Processing handled by function expenseTypeRoutes._checkDates'
-    );
-
-    let valid = recurringFlag;
-    if (!valid && !!startDate && !!endDate) {
-      valid = moment(startDate, IsoFormat).isBefore(endDate, IsoFormat);
-    }
-    let err = {
-      code: 403,
-      message: 'The dates are invalid.'
-    };
-    return valid ? Promise.resolve() : Promise.reject(err);
   }
 }
 module.exports = ExpenseTypeRoutes;
--- spec/routes/expenseTypeRoutes.spec.js
+++ spec/routes/expenseTypeRoutes.spec.js
@@ -11,12 +11,18 @@ describe('expenseTypeRoutes', () => {
   const endDate = '2019-05-16';
   const recurringFlag = false;
 
-  let expenseTypeDynamo, expenseTypeRoutes;
+  let expenseTypeDynamo, expenseTypeRoutes, databaseModify, expenseData;
 
   beforeEach(() => {
     expenseTypeRoutes = new ExpenseTypeRoutes();
     expenseTypeDynamo = jasmine.createSpyObj('expenseTypeDynamo', ['addToDB', 'updateEntryInDB']);
+    databaseModify = jasmine.createSpyObj('databaseModify', [
+      'removeFromDB',
+    ]);
+    expenseData = jasmine.createSpyObj('expenseData', ['querySecondaryIndexInDB']);
     expenseTypeRoutes.expenseTypeDynamo = expenseTypeDynamo;
+    expenseTypeRoutes.databaseModify = databaseModify;
+    expenseTypeRoutes.expenseData = expenseData;
   });
 
   describe('_add', () => {
@@ -58,6 +64,187 @@ describe('expenseTypeRoutes', () => {
       });
     }); // when DynamoDB throws an error
   }); // _add
+
+  describe('_checkDates', () => {
+    let startDate, endDate, recurringFlag, keptItsPromise;
+
+    describe('if valid date range is given', () => {
+      beforeEach(() => {
+        startDate = '1970-12-01';
+        endDate = '1970-12-31';
+        recurringFlag = false;
+        keptItsPromise = false;
+      });
+
+      it('should return a resolved promise', done => {
+        expenseTypeRoutes
+          ._checkDates(startDate, endDate, recurringFlag)
+          .then(() => {
+            keptItsPromise = true;
+            expect(keptItsPromise).toBe(true);
+            done();
+          })
+          .catch(() => done(new Error('Promise should resolve')));
+      }); // should return a resolved promise
+    }); // if valid date range is given
+
+    describe('if an invalid range is given', () => {
+      let expectedErr;
+      beforeEach(() => {
+        expectedErr = {
+          code: 403,
+          message: 'The dates are invalid.'
+        };
+        startDate = '2019-12-31';
+        endDate = '1970-12-31';
+        recurringFlag = false;
+        keptItsPromise = false;
+      });
+      it('should return a promise rejection', done => {
+        expenseTypeRoutes
+          ._checkDates(startDate, endDate, recurringFlag)
+          .then(() => done(new Error('Promise should reject')))
+          .catch(err => {
+            keptItsPromise = true;
+            expect(err).toEqual(expectedErr);
+            done();
+          });
+      }); // should return a promise rejection
+    }); // if an invalid range is given
+
+    describe('if recurring flag is undefined and startDate is undefined', () => {
+
+      beforeEach(() => {
+        endDate = '1970-12-31';
+        keptItsPromise = false;
+      });
+
+      it('should return a resolved promise', done => {
+        expenseTypeRoutes
+          ._checkDates(undefined, endDate, undefined)
+          .catch( err => {
+            expect(err).toEqual({
+              code: 403,
+              message: 'The dates are invalid.'
+            });
+            done();
+          });
+      }); // should return a resolved promise
+    }); // if recurring flag is undefined and startDate is undefined
+
+    describe('if recurring flag is undefined and endDate is undefined', () => {
+
+      beforeEach(() => {
+        startDate = '1970-12-01';
+        keptItsPromise = false;
+      });
+
+      it('should return a resolved promise', done => {
+        expenseTypeRoutes
+          ._checkDates(startDate, undefined, undefined)
+          .catch( err => {
+            expect(err).toEqual({
+              code: 403,
+              message: 'The dates are invalid.'
+            });
+            done();
+          });
+      }); // should return a resolved promise
+    }); // if recurring flag is undefined and startDate is undefined
+  }); // _checkDates
+
+  describe('_checkFields', () => {
+    let expenseType;
+
+    describe('if a vaild expense type is given', () => {
+      let keptItsPromise;
+      beforeEach(() => {
+        expenseType = { id, budgetName, budget, odFlag, description, startDate, endDate, recurringFlag };
+        keptItsPromise = false;
+      });
+
+      it('should return a resolved promise', done => {
+        expenseTypeRoutes
+          ._checkFields(expenseType)
+          .then(() => {
+            keptItsPromise = true;
+            expect(keptItsPromise).toBe(true);
+            done();
+          })
+          .catch(() => done(new Error('Promise should resolve')));
+      }); // should return a resolved promise
+    }); // if a vaild expense type is given
+    describe('if a misformed expense type object is given', () => {
+      let expectedErr;
+      beforeEach(() => {
+        expectedErr = {
+          code: 403,
+          message: 'One of the required fields is empty.'
+        };
+        expenseType = { budgetName, budget, odFlag, description, startDate, endDate, recurringFlag };
+      });
+      it('should return a promise rejection', done => {
+        expenseTypeRoutes
+          ._checkFields(expenseType)
+          .then(() => done(new Error('Promise should reject')))
+          .catch(err => {
+            expect(err).toEqual(expectedErr);
+            done();
+          });
+      }); // should return a promise rejection
+    }); // if a misformed expense type object is given
+  }); // _checkFields
+
+  describe('_delete', () => {
+
+    let expenseTypeData;
+
+    beforeEach(() => {
+      expenseTypeData = {
+        id: 'expensetypeID',
+        budgetName: ' ',
+        odFlag: false,
+        endDate: ' ',
+        budget: 'NaN',
+        requiredFlag: true,
+        recurringFlag: false,
+        isInactive: false,
+        startDate: ' ',
+        description: ' ',
+        categories: []
+      };
+    });
+
+    describe('when there are no expenses with this expense type', () => {
+
+      beforeEach(() => {
+        expenseData.querySecondaryIndexInDB.and.returnValue([]);
+        databaseModify.removeFromDB.and.returnValue(expenseTypeData);
+      });
+
+      it('should delete the expense type', () => {
+        expenseTypeRoutes._delete('expenseTypeId').then( data => {
+          expect(data).toEqual(new ExpenseType(expenseTypeData));
+        });
+      }); // should delete the expense type
+    }); // when there are no expenses with this expense type
+
+    describe('when expenses exist for this expense type', () => {
+
+      beforeEach(() => {
+        expenseData.querySecondaryIndexInDB.and.returnValue(['some-random-expense']);
+      });
+
+      it('should delete the expense type', () => {
+        expenseTypeRoutes._delete('expenseTypeId').catch( err => {
+          expect(err).toEqual({
+            code: 403,
+            message: 'Expense Type can not be deleted if they have expenses'
+          });
+        });
+      }); // should delete the expense type
+    }); // when expenses exist for this expense type
+  }); // _delete
 
   describe('_update', () => {
     let expectedExpenseType, data;
@@ -113,94 +300,4 @@ describe('expenseTypeRoutes', () => {
       });
     }); // when DynamoDB update throws an error
   }); // _update
-
-  describe('_checkFields', () => {
-    let expenseType;
-
-    describe('if a vaild expense type is given', () => {
-      let keptItsPromise;
-      beforeEach(() => {
-        expenseType = { id, budgetName, budget, odFlag, description, startDate, endDate, recurringFlag };
-        keptItsPromise = false;
-      });
-
-      it('should return a resolved promise', done => {
-        expenseTypeRoutes
-          ._checkFields(expenseType)
-          .then(() => {
-            keptItsPromise = true;
-            expect(keptItsPromise).toBe(true);
-            done();
-          })
-          .catch(() => done(new Error('Promise should resolve')));
-      }); // should return a resolved promise
-    }); // if a vaild expense type is given
-    describe('if a misformed expense type object is given', () => {
-      let expectedErr;
-      beforeEach(() => {
-        expectedErr = {
-          code: 403,
-          message: 'One of the required fields is empty.'
-        };
-        expenseType = { budgetName, budget, odFlag, description, startDate, endDate, recurringFlag };
-      });
-      it('should return a promise rejection', done => {
-        expenseTypeRoutes
-          ._checkFields(expenseType)
-          .then(() => done(new Error('Promise should reject')))
-          .catch(err => {
-            expect(err).toEqual(expectedErr);
-            done();
-          });
-      }); // should return a promise rejection
-    }); // if a misformed expense type object is given
-  }); // _checkFields
-
-  describe('_checkDates', () => {
-    let startDate, endDate, recurringFlag, keptItsPromise;
-
-    describe('if valid date range is given', () => {
-      beforeEach(() => {
-        startDate = '1970-12-01';
-        endDate = '1970-12-31';
-        recurringFlag = false;
-        keptItsPromise = false;
-      });
-
-      it('should return a resolved promise', done => {
-        expenseTypeRoutes
-          ._checkDates(startDate, endDate, recurringFlag)
-          .then(() => {
-            keptItsPromise = true;
-            expect(keptItsPromise).toBe(true);
-            done();
-          })
-          .catch(() => done(new Error('Promise should resolve')));
-      }); // should return a resolved promise
-    }); // if valid date range is given
-
-    describe('if an invalid range is given', () => {
-      let expectedErr;
-      beforeEach(() => {
-        expectedErr = {
-          code: 403,
-          message: 'The dates are invalid.'
-        };
-        startDate = '2019-12-31';
-        endDate = '1970-12-31';
-        recurringFlag = false;
-        keptItsPromise = false;
-      });
-      it('should return a promise rejection', done => {
-        expenseTypeRoutes
-          ._checkDates(startDate, endDate, recurringFlag)
-          .then(() => done(new Error('Promise should reject')))
-          .catch(err => {
-            keptItsPromise = true;
-            expect(err).toEqual(expectedErr);
-            done();
-          });
-      }); // should return a promise rejection
-    }); // if an invalid range is given
-  }); // _checkDates
 }); // employeeRoutes
