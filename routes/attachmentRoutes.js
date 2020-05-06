945044df7b932c2a097e373408d38af55fcf9010
austinjl <alam@consultwithcase.com>
2020-05-05 20:26:32 -0400

    936-Refactor-and-document-attachmentRoutes.js: Refactor attachmentRoutes

--- routes/attachmentRoutes.js
+++ routes/attachmentRoutes.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const databaseModify = require('../js/databaseModify');
-const expenseDynamo = new databaseModify('expenses');
 const _ = require('lodash');
 const Logger = require('../js/Logger');
-const logger = new Logger('attachmentRoutes');
-
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const getUserInfo = require('../js/GetUserInfoMiddleware').getUserInfo;
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
-// Authentication middleware. When used, the
-// Access Token must exist and be verified against
-// the Auth0 JSON Web Key Set
+
+const logger = new Logger('attachmentRoutes');
+const expenseDynamo = new databaseModify('expenses');
+
+// Authentication middleware. When used, the Access Token must exist and be verified against the Auth0 JSON Web Key Set
 const checkJwt = jwt({
-  // Dynamically provide a signing key
-  // based on the kid in the header and
-  // the signing keys provided by the JWKS endpoint.
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS
+  // endpoint.
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
@@ -35,7 +33,7 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 const BUCKET = `case-consulting-expense-app-attachments-${STAGE}`;
 
-var storage = multerS3({
+const storage = multerS3({
   s3: s3,
   bucket: BUCKET,
   acl: 'bucket-owner-full-control',
@@ -46,17 +44,18 @@ var storage = multerS3({
   }
 });
 
-var limits = {
+// file limits
+const limits = {
   files: 1, // allow only 1 file per request
   fileSize: 6 * 1024 * 1024 // 6 MB (max file size)
 };
 
-/**
- * Filter valid mimetypes
- */
-var fileFilter = function(req, file, cb) {
-  logger.log(1, 'fileFilter', `Attempting to upload attachment ${file.originalname}`);
+// filter valid mimetypes
+const fileFilter = function(req, file, cb) {
+  // log method
+  logger.log(2, 'fileFilter', `Attempting to validate type of attachment ${file.originalname}`);
 
+  // compute method
   // Content types that are allowed to be uploaded
   const ALLOWED_CONTENT_TYPES = [
     'application/pdf', //.pdf
@@ -71,102 +70,70 @@ var fileFilter = function(req, file, cb) {
   ];
 
   if (_.includes(ALLOWED_CONTENT_TYPES, file.mimetype)) {
-    // allow supported image files
-    logger.log(2, 'fileFilter', `Mimetype ${file.mimetype} accepted`);
+    // valid file type
+    logger.log(2, 'fileFilter', `Successfully validated Mimetype ${file.mimetype} of attachment ${file.originalname}`);
 
     cb(null, true);
   } else {
-    // throw error for invalid files
-    logger.log(1, 'fileFilter', `Mimetype ${file.mimetype} rejected`);
+    // invalid file type
+    logger.log(2, 'fileFilter', `Failed to validate Mimetype ${file.mimetype} of attachment ${file.originalname}`);
 
     cb(new Error(`Invalid file type ${file.mimetype}. View help menu for a list of valid file types.`));
   }
 };
 
-var upload = multer({
+// s3 fild upload multer
+const upload = multer({
   storage: storage,
   limits: limits,
   fileFilter: fileFilter
 }).single('receipt');
 
 class Attachment {
+
   constructor() {
-    this.expenseData = expenseDynamo;
     this._router = express.Router();
-    this._router.post('/:employeeId/:expenseId', checkJwt, getUserInfo, this.uploadAttachmentToS3.bind(this));
-    this.router.get('/:employeeId/:expenseId', checkJwt, getUserInfo, this.getAttachmentFromS3.bind(this));
-    this.router.delete('/:employeeId/:expenseId/:receipt',
-      checkJwt,
-      getUserInfo,
+    this._checkJwt = checkJwt;
+    this._getUserInfo = getUserInfo;
+    this._router.post(
+      '/:employeeId/:expenseId',
+      this._checkJwt,
+      this._getUserInfo,
+      this.uploadAttachmentToS3.bind(this)
+    );
+    this._router.get('/:employeeId/:expenseId', this._checkJwt, this._getUserInfo, this.getAttachmentFromS3.bind(this));
+    this._router.delete(
+      '/:employeeId/:expenseId/:receipt',
+      this._checkJwt,
+      this._getUserInfo,
       this.deleteAttachmentFromS3.bind(this)
     );
-  }
-
-  get router() {
-    return this._router;
-  }
+    this.expenseDynamo = expenseDynamo;
+  } // constructor
 
   /**
-   * Uploads an object to S3.
-   */
-  uploadAttachmentToS3(req, res) {
-
-    upload(req, res, (err) => {
-      if (err) {
-        // if there is an error from validating the file
-        logger.log(1, 'uploadAttachmentToS3', 'Failed to upload file');
-
-        let error = {
-          code: 403,
-          message:
-            `${err.message}`
-        };
-        res.status(error.code).send(error);
-        return error;
-      } else {
-        // successfully validated file
-        logger.log(1, 'uploadAttachmentToS3',
-          `Successfully uploaded attachment ${req.file.originalname} with file key ${req.file.key}`,
-          `to S3 bucket ${req.file.bucket}`
-        );
-
-        res.status(200).send(req.file);
-        return req.file;
-      }
-    });
-  }
-
-  /**
-   * Gets an object from S3.
-   */
-  async getAttachmentFromS3(req, res) {
-    logger.log(1, 'getAttachmentFromS3', `Getting attachment for expense ${req.params.expenseId}`);
-
-    let expense = await this.expenseData.getEntry(req.params.expenseId);
-    let fileExt = expense.receipt;
-    let filePath = `${req.params.employeeId}/${req.params.expenseId}/${fileExt}`;
-    var params = { Bucket: BUCKET, Key: filePath, Expires: 60 };
-    s3.getSignedUrl('getObject', params, (err, data) => {
-      if (err) throw err;
-      else res.status(200).send(data);
-    });
-  }
-
-  /**
-   * Deletes an object from S3.
+   * Deletes an attachment from S3.
+   *
+   * @param req - api request
+   * @param res - api response
+   * @return Object - file deleted from s3
    */
   async deleteAttachmentFromS3(req, res) {
-    logger.log(1, 'deleteAttachmentFromS3', `Attempting to delete attachment for expense ${req.params.expenseId}`);
+    // log method
+    logger.log(2, 'deleteAttachmentFromS3',
+      `Attempting to delete attachment ${req.params.receipt} for expense ${req.params.expenseId}`
+    );
 
     // set up params
     let fileExt = req.params.receipt;
     let filePath = `${req.params.employeeId}/${req.params.expenseId}/${fileExt}`;
     let params = { Bucket: BUCKET, Key: filePath};
+
     // make delete call to s3
     s3.deleteObject(params, (err, data) => {
       if (err) {
-        // if there is an error deleting the file
-        logger.log(1, 'deleteAttachmentFromS3',
+        // log error
+        logger.log(2, 'deleteAttachmentFromS3',
           `Failed to delete attachment for expense ${req.params.expenseId} from S3 ${filePath}`
         );
 
@@ -176,18 +143,126 @@ class Attachment {
             `${err.message}`
         };
 
+        // send error status
         res.status(error.code).send(error);
+
+        // return error
         return error;
       } else {
-        // successfully deleted the file
-        logger.log(1, 'deleteAttachmentFromS3',
+        // log success
+        logger.log(2, 'deleteAttachmentFromS3',
           `Successfully deleted attachment for expense ${req.params.expenseId} from S3 ${filePath}`
         );
 
+        // send successful 200 status
         res.status(200).send(data);
+
+        // return file read
+        return data;
       }
     });
-  }
-}
+  } // deleteAttachmentFromS3
+
+  /**
+   * Gets an attachment from S3.
+
+   * @param req - api request
+   * @param res - api response
+   * @return Object - file read from s3
+   */
+  async getAttachmentFromS3(req, res) {
+    // log method
+    logger.log(2, 'getAttachmentFromS3', `Getting attachment for expense ${req.params.expenseId}`);
+
+    // compute method
+    let expense = await this.expenseDynamo.getEntry(req.params.expenseId);
+    let fileExt = expense.receipt;
+    let filePath = `${req.params.employeeId}/${req.params.expenseId}/${fileExt}`;
+    let params = { Bucket: BUCKET, Key: filePath, Expires: 60 };
+    s3.getSignedUrl('getObject', params, (err, data) => {
+      if (err) {
+        // log error
+        logger.log(2, 'getAttachmentFromS3', 'Failed to read attachment');
+
+        let error = {
+          code: 403,
+          message:
+            `${err.message}`
+        };
+
+        // send error status
+        res.status(error.code).send(error);
+
+        // return error
+        return error;
+      } else {
+        // log success
+        logger.log(2, 'getAttachmentFromS3', `Successfully read attachment from s3 ${filePath}`);
+
+        // send successful 200 status
+        res.status(200).send(data);
+
+        // return file read
+        return data;
+      }
+    });
+  } // getAttachmentFromS3
+
+  /**
+   * Returns the instace express router.
+   *
+   * @return Router Object - express router
+   */
+  get router() {
+    // log method
+    logger.log(2, 'router', 'Getting router');
+
+    return this._router;
+  } // router
+
+  /**
+   * Uploads an attachment to s3.
+   *
+   * @param req - api request
+   * @param res - api response
+   * @return Object - file uploaded
+   */
+  uploadAttachmentToS3(req, res) {
+    // log method
+    logger.log(2, 'uploadAttachmentToS3', `Attempting to upload attachment for expense ${req.params.expenseId}`);
+
+    // compute method
+    upload(req, res, (err) => {
+      if (err) {
+        // log error
+        logger.log(2, 'uploadAttachmentToS3', 'Failed to upload file');
+
+        let error = {
+          code: 403,
+          message:
+            `${err.message}`
+        };
+
+        // send error status
+        res.status(error.code).send(error);
+
+        // return error
+        return error;
+      } else {
+        // log success
+        logger.log(2, 'uploadAttachmentToS3',
+          `Successfully uploaded attachment ${req.file.originalname} with file key ${req.file.key}`,
+          `to S3 bucket ${req.file.bucket}`
+        );
+
+        // send successful 200 status
+        res.status(200).send(req.file);
+
+        // return file uploaded
+        return req.file;
+      }
+    });
+  } // uploadAttachmentToS3
+} // Attachment
 
 module.exports = Attachment;
