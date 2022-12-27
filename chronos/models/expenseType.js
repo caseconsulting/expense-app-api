372b170bb8330126d5f32ca0353b2b6bf6bdb379
despinoza17 <despi17@vt.edu>
2022-12-27 12:45:35 -0500

    1969: Migrated budget.js and expenseType.js

--- chronos/models/expenseType.js
+++ chronos/models/expenseType.js
@@ -1,7 +1,5 @@
-const moment = require('moment-timezone');
-moment.tz.setDefault('America/New_York');
-const ISOFORMAT = 'YYYY-MM-DD';
 const _ = require('lodash');
+const dateUtils = require('../js/dateUtils');
 
 /**
  * ExpenseType model
@@ -54,7 +52,7 @@ class ExpenseType {
    * Check if a date is in the expense type date range. Returns true if the expense type is recurring or the date is
    * between the expense type start and end date. Returns false otherwise.
    *
-   * @param dateStr - moment of date to be checked
+   * @param dateStr - date to be checked
    * @return Boolean - date is in range
    */
   isDateInRange(dateStr) {
@@ -63,10 +61,9 @@ class ExpenseType {
     } else if (_.isNil(dateStr)) {
       return false;
     } else {
-      let date = moment(dateStr, ISOFORMAT);
-      let start = moment(this.startDate, ISOFORMAT);
-      let end = moment(this.endDate, ISOFORMAT);
-      return date.isBetween(start, end, null, '[]');
+      let start = this.startDate;
+      let end = this.endDate;
+      return dateUtils.isBetween(dateStr, start, end, null, '[]');
     }
   } // isDateInRange
 
