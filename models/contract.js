22847bcdc52c1dcbc3c4e8a3c0205d3b2d1260e9
Chad Martin <martincc@dukes.jmu.edu>
2023-01-02 15:56:36 -0500

    POR-1984: add a model for contracts

---
+++ models/contract.js
@@ -0,0 +1,63 @@
+const _ = require('lodash');
+
+/**
+ * Contract model
+ *
+ * Fields:
+ * - contractName
+ * - primeName
+ * - projects
+ * - popStartDate
+ * - popEndDate
+ * - costType
+ */
+
+class Contract {
+  constructor(data) {
+    this.setRequiredAttribute(data, 'contractName');
+    this.setRequiredAttribute(data, 'primeName');
+    this.setRequiredAttribute(data, 'projects');
+    this.setRequiredAttribute(data, 'popStartDate');
+    this.setRequiredAttribute(data, 'popEndDate');
+    this.setRequiredAttribute(data, 'costType');
+  } // constructor
+
+  /**
+   * Checks if a value is empty. Returns true if the value is null or an empty/blank string.
+   *
+   * @param value - value to check
+   * @return boolean - value is empty
+   */
+  _isEmpty(value) {
+    return _.isNil(value) || (_.isString(value) && value.trim().length === 0);
+  } // isEmpty
+
+  /**
+   * Sets a contract attribute. If the data attribute is empty, sets the attribute to the default value.
+   *
+   * @param data - contract data
+   * @param attribute - contract attribute
+   * @param defaultValue - default value
+   */
+  setRequiredAttribute(data, attribute, defaultValue) {
+    if (!this._isEmpty(data[attribute])) {
+      this[attribute] = data[attribute];
+    } else {
+      this[attribute] = defaultValue;
+    }
+  } // setRequiredAttribute
+
+  /**
+   * Sets a contract attribute if it is not null or an empty/blank string.
+   *
+   * @param data - contract data
+   * @param attribute - contract attribute
+   */
+  setOptionalAttribute(data, attribute) {
+    if (!this._isEmpty(data[attribute])) {
+      this[attribute] = data[attribute];
+    }
+  } // setOptionalAttribute
+} // Contract
+
+module.exports = Contract;
