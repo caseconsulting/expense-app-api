5ada7c937bfc73f996849443eaf8994ae7ddcca2
despinoza17 <despi17@vt.edu>
2023-06-02 10:53:50 -0400

    POR-2231: Fixed lambda role permissions for data synce lambda function

--- portal-data-sync/app.js
+++ portal-data-sync/app.js
@@ -1,8 +1,60 @@
 const axios = require('axios');
 const AWS = require('aws-sdk');
+const DatabaseModify = require('./js/databaseModify');
 
 const paramName = '/BambooHR/APIKey';
 const baseURL = 'https://api.bamboohr.com/api/gateway.php/consultwithcase/v1';
+/**
+ * List of employee fields to synchronize:
+ *
+ * employee number
+ * name
+ * address
+ * phone number (default is fine to not show)
+ * date of birth
+ * place of birth
+ * phone number
+ * gender
+ * veteran status
+ * ethnicity
+ * eeo job category
+ * hire date
+ * status
+ * social media info
+ * job role
+ *
+ *
+ */
+
+const fieldNames = [
+  { field: 'employeeNumber', bambooHRFieldName: 'employeeNumber', casePortalFieldName: 'employeeNumber' },
+  { field: 'firstName', bambooHRFieldName: 'firstName', casePortalFieldName: 'firstName' },
+  { field: 'lastName', bambooHRFieldName: 'lastName', casePortalFieldName: 'lastName' },
+  { field: 'middleName', bambooHRFieldName: 'middleName', casePortalFieldName: 'middleName' },
+  { field: 'address1', bambooHRFieldName: 'address1', casePortalFieldName: 'currentStreet' },
+  { field: 'city', bambooHRFieldName: 'city', casePortalFieldName: 'currentCity' },
+  { field: 'state', bambooHRFieldName: 'state', casePortalFieldName: 'currentState' },
+  { field: 'country', bambooHRFieldName: 'country', casePortalFieldName: 'currentCountry' },
+  { field: 'homePhone', bambooHRFieldName: 'homePhone', casePortalFieldName: 'privatePhoneNumbers' },
+  { field: 'workPhone', bambooHRFieldName: 'workPhone', casePortalFieldName: 'privatePhoneNumbers' },
+  { field: 'mobilePhone', bambooHRFieldName: 'mobilePhone', casePortalFieldName: 'privatePhoneNumbers' },
+  { field: 'workPhoneExtenstion', bambooHRFieldName: 'workPhoneExtension', casePortalFieldName: 'privatePhoneNumbers' },
+  { field: 'dateOfBirth', bambooHRFieldName: 'dateOfBirth', casePortalFieldName: 'birthday' },
+  { field: 'placeOfBirth', bambooHRFieldName: 'placeOfBirth', casePortalFieldName: ['city', 'st', 'country'] }, //
+  { field: 'gender', bambooHRFieldName: 'gender', casePortalFieldName: 'eeoGender' },
+  // {field: 'veteranStatus'},
+  {
+    field: 'ethnicity',
+    bambooHRFieldName: 'ethnicity',
+    casePortalFieldName: ['eeoHispanicOrLatino', 'eeoRaceOrEthnicity']
+  },
+  { field: 'jobCategory', bambooHRFieldName: 'eeo', casePortalFieldName: 'eeoJobCategory' },
+  { field: 'hireDate', bambooHRFieldName: 'hireDate', casePortalFieldName: 'hireDate' },
+  { field: 'workStatus', bambooHRFieldName: ['employmentHistoryStatus', 'status'], casePortalFieldName: 'workStatus' },
+  { field: 'twitter', bambooHRFieldName: 'twitterFeed', casePortalFieldName: 'twitter' },
+  { field: 'linkedIn', bambooHRFieldName: 'linkedIn', casePortalFieldName: 'linkedIn' },
+  { field: 'jobRole', bambooHRFieldName: 'jobTitle', casePortalFieldName: 'jobRole' }
+];
 
 function getSSM() {
   AWS.config.update({ region: 'us-east-1' });
@@ -23,15 +75,32 @@ async function getKey() {
 async function getBambooHREmployeeData() {
   const key = await getKey();
   const options = {
-    method: 'GET',
-    url: baseURL + '/meta/users',
+    method: 'POST',
+    url: baseURL + '/reports/custom',
+    params: { format: 'JSON', onlyCurrent: 'true' },
     auth: {
       username: key,
       password: ''
-    }
+    },
+    data: { fields: ['firstName'] }
   };
   const employeeData = await axios(options);
-  console.info(employeeData);
+  return employeeData.data.employees;
+}
+
+async function getCasePortalEmployeeData() {
+  let employeeDynamo = new DatabaseModify('employees');
+  let employeeSensitiveDynamo = new DatabaseModify('employees-sensitive');
+  let employees = await employeeDynamo.getAllEntriesInDB();
+  let employeesSensitive = await employeeSensitiveDynamo.getAllEntriesInDB();
+  console.log(employees);
+  console.log(employeesSensitive);
+}
+
+async function detectAndSync() {
+  await getBambooHREmployeeData();
+  await getCasePortalEmployeeData();
+  console.log(fieldNames);
 }
 
 /**
@@ -40,7 +109,7 @@ async function getBambooHREmployeeData() {
  * @param event - request
  */
 async function handler() {
-  await getBambooHREmployeeData();
+  await detectAndSync();
 } // handler
 
 module.exports = { handler };
---
+++ portal-data-sync/models/budget.js
@@ -0,0 +1,112 @@
+const _ = require('lodash');
+const dateUtils = require('../js/dateUtils');
+const ISOFORMAT = 'YYYY-MM-DD';
+
+/**
+ * Budget model
+ *
+ * Fields:
+ * - id
+ * - expenseTypeId
+ * - employeeId
+ * - reimbursedAmount
+ * - pendingAmount
+ * - fiscalStartDate
+ * - fiscalEndDate
+ * - amount
+ */
+
+class Budget {
+  constructor(data) {
+    // required attributes
+    this.setRequiredAttribute(data, 'id');
+    this.setRequiredAttribute(data, 'expenseTypeId');
+    this.setRequiredAttribute(data, 'employeeId');
+    this.setRequiredNumberAttribute(data, 'reimbursedAmount', undefined, 2);
+    this.setRequiredNumberAttribute(data, 'pendingAmount', undefined, 2);
+    this.setRequiredAttribute(data, 'fiscalStartDate');
+    this.setRequiredAttribute(data, 'fiscalEndDate');
+    this.setRequiredNumberAttribute(data, 'amount', undefined, 2);
+  } // constructor
+
+  /**
+   * Check if a date is in the budget date range. Returns true if the date is between the budget fiscal start start and
+   * end date. Returns false otherwise.
+   *
+   * @param dateStr - date to be checked
+   * @return Boolean - date is in range
+   */
+  isDateInRange(dateStr) {
+    let start = dateUtils.format(this.fiscalStartDate, null, ISOFORMAT);
+    let end = dateUtils.format(this.fiscalEndDate, null, ISOFORMAT);
+    return dateUtils.isBetween(dateStr, start, end, 'day', '[]');
+  } // isDateInRange
+
+  /**
+   * Checks if a value is empty. Returns true if the value is null or an empty/blank string.
+   *
+   * @param value - value to check
+   * @return boolean - value is empty
+   */
+  _isEmpty(value) {
+    return _.isNil(value) || (_.isString(value) && value.trim().length === 0);
+  } // _isEmpty
+
+  /**
+   * Sets an employee attribute if it is not null or an empty/blank string.
+   *
+   * @param data - employee data
+   * @param attribute - employee attribute
+   */
+  setOptionalAttribute(data, attribute) {
+    if (!this._isEmpty(data[attribute])) {
+      this[attribute] = data[attribute];
+    }
+  } // setOptionalAttribute
+
+  /**
+   * Sets an employee attribute number if it is not null or an empty/blank string.
+   *
+   * @param data - employee data
+   * @param attribute - employee attribute
+   * @param fixed - decimal places to fix value
+   */
+  setOptionalNumberAttribute(data, attribute, fixed) {
+    if (!this._isEmpty(data[attribute])) {
+      this[attribute] = fixed ? Number(Number(data[attribute]).toFixed(fixed)) : Number(data[attribute]);
+    }
+  } // setOptionalNumberAttribute
+
+  /**
+   * Sets an employee attribute. If the data attribute is empty, sets the attribute to the default value.
+   *
+   * @param data - employee data
+   * @param attribute - employee attribute
+   * @param defaultValue - default value
+   */
+  setRequiredAttribute(data, attribute, defaultValue) {
+    if (!this._isEmpty(data[attribute])) {
+      this[attribute] = data[attribute];
+    } else {
+      this[attribute] = defaultValue;
+    }
+  } // setRequiredAttribute
+
+  /**
+   * Sets an employee attribute number. If the data attribute is empty, sets the attribute to the default value.
+   *
+   * @param data - employee data
+   * @param attribute - employee attribute
+   * @param defaultValue - default value
+   * @param fixed - decimal places to fix value
+   */
+  setRequiredNumberAttribute(data, attribute, defaultValue, fixed) {
+    if (!this._isEmpty(data[attribute])) {
+      this[attribute] = fixed ? Number(Number(data[attribute]).toFixed(fixed)) : Number(data[attribute]);
+    } else {
+      this[attribute] = defaultValue;
+    }
+  } // setRequiredNumberAttribute
+} // Budget
+
+module.exports = Budget;
