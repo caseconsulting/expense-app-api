fa2d6a1bc5ea0a0e5e305ca66a15b2c58d8d19fd
Madeline Burns <madelineburns@Madelines-Laptop.local>
2024-07-03 13:53:23 -0400

    POR-2628-Autofilled-Google-API-for-POB: Added google api call for cities

--- routes/googleMapsRoutes.js
+++ routes/googleMapsRoutes.js
@@ -16,6 +16,7 @@ class GoogleMapRoutes {
     this._getUserInfo = getUserInfo;
     this._router.get('/getLocation/:location', this._checkJwt, this._getUserInfo, this._getLocation.bind(this));
     this._router.get('/getZipCode/:addressId', this._checkJwt, this._getUserInfo, this._getZipCode.bind(this));
+    this._router.get('/getCity/:location', this._checkJwt, this._getUserInfo, this._getCity.bind(this));
   } // constructor
 
   /**
@@ -67,7 +68,7 @@ class GoogleMapRoutes {
   async _getZipCode(req, res) {
     let addressId = req.params.addressId;
     let googleKey = process.env.NODE_ENV_GOOGLE_MAPS_KEY;
-    let baseURL = `https://maps.googleapis.com/maps/api/place/details/json?key=${googleKey}`;
+    let baseURL = `https://maps.googleapis.com/maps/api/place/place/json?key=${googleKey}`;
     logger.log(1, '_getZipCode', `Attempting to get requested zip code for ${addressId}`);
     var config = {
       method: 'get',
@@ -85,6 +86,35 @@ class GoogleMapRoutes {
       this._sendError(res, error);
     }
   } //_getZipCode
+
+  /**
+   * Used to return a list of 'predictions' of cities according to the user's input.
+   *
+   * @param req - contains string of location typed by user
+   * @param res - returns Google Maps API object according to the user's input in the city field
+   */
+  async _getCity(req, res) {
+    let city = req.params.location;
+    city = city.replace(' ', '+');
+    let googleKey = process.env.NODE_ENV_GOOGLE_MAPS_KEY;
+    let baseURL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${googleKey}`;
+    logger.log(1, '_getLocation', `Attempting to get requested city for ${city}`);
+    var config = {
+      method: 'get',
+      url: `${baseURL}&types=locality&input=${city}`
+    };
+    try {
+      let response = await this.callAxios(config);
+      logger.log(1, '_getLocation', 'Successfully obtained location(s)!');
+      res.status(200).send(response.data);
+    } catch (err) {
+      let error = {
+        code: 400,
+        message: err.message
+      };
+      this._sendError(res, error);
+    }
+  } //_getLocation
 
   /**
    *
