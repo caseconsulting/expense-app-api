00b9e102d4b68386e72c4681b2a53fc418c6b4ee
zramsis <zramsis@gmail.com>
2026-01-08 11:23:56 -0500

    add lambda function for reseting custom hours

---
+++ crons/leaderboard/customHours/customHours.js
@@ -0,0 +1,44 @@
+const Logger = require(process.env.AWS ? 'Logger' : '../../js/Logger');
+const { getEmployees, asyncForEach } = require(process.env.AWS ? 'utils' : '../../js/utils');
+const logger = new Logger('LeaderboardCron');
+const STAGE = process.env.STAGE;
+
+
+async function resetCustomHours() {
+  try {
+    logger.log(1, 'resetCustomHours', 'Attempting to reset custom hours');
+
+    let employees = await getEmployees();
+
+    await asyncForEach(employees, async (employee) => {
+      try {
+        logger.log(1, 'resetCustomHours', `Resetting custom hours for employee ${employee.id}`);
+        if (employee.preferences?.customNeeded) {
+          delete employee.preferences.customNeeded;
+          await employee.save();
+        }
+        logger.log(1, 'resetCustomHours', `Successfully reset custom hours for employee ${employee.id}`);
+      } catch (err) {
+        logger.log(1, 'resetCustomHours', `Failed to reset custom hours for employee ${employee.id}`);
+      }
+    });
+  } catch (err) {
+    logger.log(1, 'resetCustomHours', 'Error resetting custom hours', err);
+  }
+}
+
+/**
+ * Handler to execute lamba function.
+ *
+ * @param event - request
+ */
+async function handler(event) {
+  console.info(JSON.stringify(event));
+  if (['prod', 'test'].includes(STAGE)) {
+    await resetCustomHours();
+  } else {
+    logger.log(1, 'handler', 'Environment skipped.');
+  }
+} // handler
+
+module.exports = { handler };
