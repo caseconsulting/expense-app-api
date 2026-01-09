83a16867fc6b2fa004c01d45a7cc9665268f4943
zramsis <zramsis@gmail.com>
2026-01-09 10:58:39 -0500

    fixes

--- crons/customHours/customHours.js
+++ crons/customHours/customHours.js
@@ -2,9 +2,8 @@ const Logger = require(process.env.AWS ? 'Logger' : '../../js/Logger');
 const { getEmployees, asyncForEach } = require(process.env.AWS ? 'utils' : '../../js/utils');
 const logger = new Logger('LeaderboardCron');
 const STAGE = process.env.STAGE;
-const EmployeeRoutes = require(process.env.AWS ? 'employeeRoutes' : '../../routes/employeeRoutes');
-
-const employeeRoutes = new EmployeeRoutes();
+const DatabaseModify = require(process.env.AWS ? 'databaseModify' : '../../js/databaseModify');
+const databaseModify = new DatabaseModify('employees');
 
 async function resetCustomHours() {
   try {
@@ -12,18 +11,17 @@ async function resetCustomHours() {
 
     let employees = await getEmployees();
 
-    await asyncForEach(employees, async (employee) => {
+    for (const employee of employees) {
       try {
-        logger.log(1, 'resetCustomHours', `Resetting custom hours for employee ${employee.id}`);
         if (employee.preferences?.timesheetPreferences?.customNeeded) {
           delete employee.preferences.timesheetPreferences.customNeeded;
-          await employeeRoutes._update({ body: employee, employee: employee });
+          await databaseModify.updateAttributeInDB(employee, 'preferences');
+          logger.log(1, 'resetCustomHours', `Successfully reset custom hours for employee ${employee.id}`);
         }
-        logger.log(1, 'resetCustomHours', `Successfully reset custom hours for employee ${employee.id}`);
       } catch (err) {
         logger.log(1, 'resetCustomHours', `Failed to reset custom hours for employee ${employee.id}`);
       }
-    });
+    }
   } catch (err) {
     logger.log(1, 'resetCustomHours', 'Error resetting custom hours', err);
   }
