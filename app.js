b4b5d57fa72dc3476097089ef6fc70df9726bdb1
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2026-04-22 13:53:04 -0400

    allow for blank contracts for some reason

--- app.js
+++ app.js
@@ -67,8 +67,8 @@ const tagRoutes = new TagRoutes();
 const Settings = require('./routes/settings');
 const settings = new Settings();
 
-const AccessGroupsRoutes = require('./routes/accessGroups');
-const accessGroupRoutes = new AccessGroupsRoutes();
+const AccessRolesRoutes = require('./routes/accessRoles');
+const accessRoleRoutes = new AccessRolesRoutes();
 
 const UnanetRoutes = require('./routes/unanetRoutes');
 const unanetRoutes = new UnanetRoutes();
@@ -116,7 +116,7 @@ app.use('/highFives', highFiveRoutes.router);
 app.use('/ptoCashOuts', ptoCashOutRoutes.router);
 app.use('/settings', settings.router);
 app.use('/tags', tagRoutes.router);
-app.use('/accessGroups', accessGroupRoutes.router);
+app.use('/accessRoles', accessRoleRoutes.router);
 app.use('/unanet', unanetRoutes.router);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
